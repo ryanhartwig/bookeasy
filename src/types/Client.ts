54f40a62ab471d26848067647649b0a44d9cbe51
Ryan1431 <ryan1431@gmail.com>
2023-02-27 18:12:02 -0500

    Add details array and Details type to allow businesses to add client notes

--- src/types/Client.ts
+++ src/types/Client.ts
@@ -1,6 +1,13 @@
+interface Details {
+  phone?: number,
+  email?: string,
+  address?: string,
+  notes?: string,
+}
 export interface Client {
   id: string,
   name: string,
   email: string,
   teams: string[],
+  details: Details[],
 };
