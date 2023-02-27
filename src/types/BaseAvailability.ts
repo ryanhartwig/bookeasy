9bbcd2908502b06a1b2c91210b61c71e0b3b4913
Ryan1431 <ryan1431@gmail.com>
2023-02-27 18:15:48 -0500

    Add base availability type and sample data

---
+++ src/types/BaseAvailability.ts
@@ -0,0 +1,12 @@
+export interface BaseAvailability {
+  id: string,
+  user_id: string,
+  business_id: string,
+  monday: string,
+  tuesday: string,
+  wednesday: string,
+  thursday: string,
+  friday: string,
+  saturday: string,
+  sunday: string,
+}
