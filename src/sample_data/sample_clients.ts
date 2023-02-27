dd1d2b5955044b332fb6a036ed51db65acd84972
Ryan1431 <ryan1431@gmail.com>
2023-02-27 16:58:16 -0500

    Client type and sample data

---
+++ src/sample_data/sample_clients.ts
@@ -0,0 +1,23 @@
+import type { Client } from "../types/Client";
+
+export const sample_clients: Client[] = [{
+  id: 'stevenprice',
+  name: 'Steven Price',
+  email: 'steven.price@gmail.com',
+}, {
+  id: 'alexanderstewart',
+  name: 'Alexander Stewart',
+  email: 'alexander.stewart@gmail.com',
+}, {
+  id: 'emiliegray',
+  name: 'Emilie Gray',
+  email: 'emilie.gray@gmail.com',
+}, {
+  id: 'samuelcooper',
+  name: 'Samuel Cooper',
+  email: 'samuel.cooper@gmail.com',
+}, {
+  id: 'ethanblackwood',
+  name: 'Ethan Blackwood',
+  email: 'ethan.blackwood@gmail.com',
+}]
