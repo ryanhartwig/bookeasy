646aeddf1d86b4edc1d6c4bc138f5f52df51730b
Ryan1431 <ryan1431@gmail.com>
2023-02-27 16:46:20 -0500

    Create sample_data and types directories, add respective user data

---
+++ src/sample_data/sample_user.ts
@@ -0,0 +1,8 @@
+import type { User } from "../types/User";
+
+export const user: User = {
+  id: 'user_1',
+  own_business_id: null,
+  name: 'Christina Bronson',
+  email: 'christina.bronson@gmail.com',
+};
---
+++ src/types/User.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: string,
+  own_business_id: string | null,
+  name: string,
+  email: string,
+  phone?: number,
+}
