7b2ad0e54bd0d78ba3067150a012ce95cf52ac71
Ryan1431 <ryan1431@gmail.com>
2023-02-27 17:02:03 -0500

    Business type and sample data

---
+++ src/sample_data/sample_businesses.ts
@@ -0,0 +1,13 @@
+import type { Business } from "../types/Business";
+
+export const sample_businesses: Business[] = [{
+  id: 'christinabronson',
+  name: 'Christina Bronson',
+  email: 'christina.bronson@gmail.com',
+  phone: null,
+}, {
+  id: 'johnsonteam',
+  name: 'Johnson Team',
+  email: 'inquire.johnsonteam@gmail.com',
+  phone: 1234567890
+}];
---
+++ src/types/Business.ts
@@ -0,0 +1,6 @@
+export interface Business {
+  id: string,
+  name: string,
+  email: string | null,
+  phone: number | null,
+}
