aacb717f28c21fd2ebd7e647cddda9a89fcf0685
Ryan1431 <ryan1431@gmail.com>
2023-02-27 16:54:35 -0500

    Notifications type and sample data

---
+++ src/sample_data/sample_notifications.ts
@@ -0,0 +1,13 @@
+import type { Notification } from "../types/Notification"
+
+export const sample_notifications: Notification[] = [{
+  user_id: 'christinabronson',
+  text: 'Managing your own business? Click here to get started.',
+  link: '/home/mybusiness',
+  seen: false,
+}, {
+  user_id: 'christinabronson',
+  text: 'Thanks for joining Book it! To see a short tutorial, click here.',
+  link: '/home/tutorial',
+  seen: false,
+}];
