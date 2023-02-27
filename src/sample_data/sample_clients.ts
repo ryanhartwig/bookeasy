aa631471b3800df00378219577c507be7a3c2d40
Ryan1431 <ryan1431@gmail.com>
2023-02-27 18:08:29 -0500

    Add teams array to clients to track client - user - business relationship

--- src/sample_data/sample_clients.ts
+++ src/sample_data/sample_clients.ts
@@ -4,20 +4,25 @@ export const sample_clients: Client[] = [{
   id: 'stevenprice',
   name: 'Steven Price',
   email: 'steven.price@gmail.com',
+  teams: ['christinabronson'],
 }, {
   id: 'alexanderstewart',
   name: 'Alexander Stewart',
   email: 'alexander.stewart@gmail.com',
+  teams: ['christinabronson'],
 }, {
   id: 'emiliegray',
   name: 'Emilie Gray',
   email: 'emilie.gray@gmail.com',
+  teams: ['johnsonteam'],
 }, {
   id: 'samuelcooper',
   name: 'Samuel Cooper',
   email: 'samuel.cooper@gmail.com',
+  teams: ['johnsonteam'],
 }, {
   id: 'ethanblackwood',
   name: 'Ethan Blackwood',
   email: 'ethan.blackwood@gmail.com',
+  teams: ['johnsonteam'],
 }];
