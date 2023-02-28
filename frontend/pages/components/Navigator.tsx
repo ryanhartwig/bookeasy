828eb77d406f8d7ccecb3f4380b0aae659340a42
Ryan1431 <ryan1431@gmail.com>
2023-02-28 13:43:16 -0500

    Add book it title and temporary logo, matching figma

--- frontend/pages/components/Navigator.tsx
+++ frontend/pages/components/Navigator.tsx
@@ -1,11 +1,17 @@
 import styles from '@/styles/Home.module.css';
 
+import Image from 'next/image';
+import * as bookit from '@/assets/logo_temp.svg';
+
 export const Navigator = () => {
 
 
   return (
     <div className={styles.Navigator}>
-
+      <div className={styles.Navigator_logo}>
+        <Image className={styles.Navigator_logo_icon} src={bookit} alt="Book it logo" />
+        <span>book it.</span>
+      </div>
     </div>
   )
 }
