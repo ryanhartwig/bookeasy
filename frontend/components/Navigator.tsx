ff4c7feca13cf015da036e8b69a34aabda369622
Ryan1431 <ryan1431@gmail.com>
2023-02-28 16:49:09 -0500

    Change meta description and test Link

--- frontend/components/Navigator.tsx
+++ frontend/components/Navigator.tsx
@@ -3,6 +3,8 @@ import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
 import * as bookit from '@/assets/logo_temp.svg';
 
+import Link from 'next/link';
+
 export const Navigator = () => {
 
 
@@ -10,7 +12,7 @@ export const Navigator = () => {
     <div className={styles.Navigator}>
       <div className={styles.Navigator_logo}>
         <Image className={styles.Navigator_logo_icon} src={bookit} alt="Book it logo" />
-        <span>book it.</span>
+        <span><Link href="/home/dashboard">book it.</Link></span>
       </div>
     </div>
   )
