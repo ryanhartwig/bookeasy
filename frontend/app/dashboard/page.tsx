d6af3e172e6aeadb5fe0dd9887d605e4f9f0e40a
Ryan1431 <ryan1431@gmail.com>
2023-03-02 10:12:18 -0500

    Add date formatted information to appointment cards

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,10 +4,15 @@ import { SecondaryHeader } from "@/app/_components/SecondaryHeader"
 import { sample_appointments } from '@/app/_sample_data/sample_appointments';
 import { getCurrentWeek } from '@/app/_helpers/getCurrentWeek';
 import { Card } from '../_components/UI/Card';
+import { sample_services } from '../_sample_data/sample_services';
+import { Service } from '../_types/Service';
 
 export default function Page() {
 
   const [start, end] = getCurrentWeek();
+  const servicesMap = new Map<string, Service>();
+  const servicesIds = new Set<string>();
+
 
 
   
@@ -32,12 +37,29 @@ export default function Page() {
       <div className={styles.content}>
         <p>Today</p>
         <div className={styles.apppointments}>
-          {sample_appointments.map(app => 
+          {sample_appointments.map(app => {
+            const service = sample_services.find(s => app.service_id === s.id);
+            if (!service) return <></>
+
+            const start = new Date(app.start_date);
+
+            return (
             <Card key={app.id}>
-              <div className={styles.appointment}>
+              <div className={styles.appointment} style={{borderLeftColor: '#BA1682'}}>
+                <div className={styles.app_header}>
+                  <p>{service.name}</p>
+                  <p>{service.duration}m</p>
+                </div>
+
+                <p className={styles.alt}>
+                  {start.toDateString().split(' ').slice(0, -1).join(' ') 
+                    + ' • '
+                    + `${start.toLocaleTimeString()}`
+                  }
+                </p>
               </div>
             </Card>
-          )}
+          )})}
 
         </div>
         <p>This Week</p>
