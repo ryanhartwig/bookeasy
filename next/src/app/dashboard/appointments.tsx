bd6de635ab79a5a9954dcd126b6d902c91a77bec
Ryan1431 <ryan1431@gmail.com>
2023-03-06 17:50:21 -0500

    Add actions section and div circles

--- next/src/app/dashboard/appointments.tsx
+++ next/src/app/dashboard/appointments.tsx
@@ -24,16 +24,48 @@ export const Appointments: React.FC<AppointmentsProps> = ({appointments}) => {
         return (
         <Card key={app.id}>
           <div className={styles.appointment} style={{borderLeftColor: '#BA1682'}}>
-            <div className={styles.app_header}>
-              <p>{service.name}</p>
-              <p>{service.duration}m</p>
+            <div>
+              <div className={styles.app_header}>
+                <p>{service.name}</p>
+                <p>{service.duration}m</p>
+              </div>
+              <p className={styles.alt}>{formatTime(app.start_date)}</p>
             </div>
-
-            <p className={styles.alt}>{formatTime(app.start_date)}</p>
 
             <div className={styles.client}>
               <p className={styles.alt} style={{fontWeight: 100}}>with</p>
               <p>Steven Price</p>
+            </div>
+
+            <div className={styles.actions}>
+              {/* Left Actions */}
+              <div>
+                <div className={styles.action}>
+                  <div style={{backgroundColor: '#B54078', width: 44}}>
+
+                  </div>
+
+                  <p>Enter Session</p>
+                </div>
+              </div>
+
+              {/* Right Actions */}
+              <div>
+                <div className={styles.action}>
+                  <div style={{backgroundColor: '#55BDAA'}}>
+
+                  </div>
+
+                  <p>Calendar</p>
+                </div>
+                <div className={styles.action}>
+                  <div style={{backgroundColor: '#5482FB'}}>
+
+                  </div>
+
+                  <p>Copy Link</p>
+                </div>
+              </div>
             </div>
           </div>
         </Card>
