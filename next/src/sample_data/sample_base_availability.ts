2a41ca00270688da32c830901b52fe99f92b2b7b
Ryan1431 <ryan1431@gmail.com>
2023-03-07 18:05:31 -0500

    Refactor availability data into slices, with index based days

--- next/src/sample_data/sample_base_availability.ts
+++ next/src/sample_data/sample_base_availability.ts
@@ -4,13 +4,46 @@ export const sample_base_availability: BaseAvailability = {
   business_id: 'christinabronson',
   id: 'avail1',
   user_id: 'christinabronson',
-  monday: '9-11%13-17', 
-  tuesday: '',
-  wednesday: '',
-  thursday: '9-11%13-17',
-  friday: '',
-  saturday: '',
-  sunday: '',
+  slices: [
+    {
+      business_id: 'christinabronson',
+      user_id: 'christinabronson',
+      day: 0,
+      start: '8:00',
+      end: '12:00',
+    }, {
+      business_id: 'christinabronson',
+      user_id: 'christinabronson',
+      day: 1,
+      start: '8:00',
+      end: '12:00',
+    }, {
+      business_id: 'christinabronson',
+      user_id: 'christinabronson',
+      day: 1,
+      start: '8:00',
+      end: '12:00',
+    }, {
+      business_id: 'christinabronson',
+      user_id: 'christinabronson',
+      day: 2,
+      start: '13:00',
+      end: '17:00',
+    }, {
+      business_id: 'christinabronson',
+      user_id: 'christinabronson',
+      day: 3,
+      start: '8:00',
+      end: '12:00',
+    }, {
+      business_id: 'christinabronson',
+      user_id: 'christinabronson',
+      day: 4,
+      start: '8:00',
+      end: '12:00',
+    },
+  ],
   recurring: [],
   time_off: [],
 }
+
