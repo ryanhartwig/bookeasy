ec869cb4fe381c7f58fa9024ddd350dc6e6829a9
Ryan1431 <ryan1431@gmail.com>
2023-03-07 11:50:41 -0500

    Set up and align hours component to display hours for each day

--- next/src/app/dashboard/hourly.tsx
+++ next/src/app/dashboard/hourly.tsx
@@ -1,10 +1,16 @@
 import styles from './dashboard.module.scss';
+import { Hours } from './hours';
 
 export const Hourly = () => {
 
+  const days = Array(7).fill(true);
+  
   return (
     <div className={styles.hourly}>
+      {days.map((_, i) => {
 
+        return <Hours key={i} />
+      })}
     </div>
   )
 }
---
+++ next/src/app/dashboard/hours.tsx
@@ -0,0 +1,10 @@
+import styles from './dashboard.module.scss';
+
+export const Hours = () => {
+
+  return (
+    <div className={styles.hours}>
+
+    </div>
+  )
+}
