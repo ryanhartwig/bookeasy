4688c37da4ddad0a9aee6ba658d35aab0a3f2995
Ryan1431 <ryan1431@gmail.com>
2023-03-07 13:19:13 -0500

    Show hour text in respective blocks

--- next/src/app/dashboard/hours.tsx
+++ next/src/app/dashboard/hours.tsx
@@ -12,13 +12,30 @@ Formula to convert hours / mins into index:
 
 export const Hours = () => {
   const blocks = new Array(96).fill(true);
+
   
   return (
     <div className={styles.hours}>
       {blocks.map((_, i) => {
         const borderBottomColor = i % 4 === 3 ? '#A3A3A3' : '';
+
+        const isHour = i % 4 === 0;
+        let hourIndex = 0;
+        let hour = 0;
+        let period = '';
+        if (isHour) {
+          hourIndex = i / 4;
+          hour = hourIndex % 12 === 0 ? 12 : hourIndex % 12;
+          period = hourIndex < 12 ? 'am' : 'pm';
+        }
+
+
+        
+        
+
         return (
           <div key={i} className={styles.block} style={{borderBottomColor}}>
+            {isHour && <p>{hour} {period}</p>}
           </div>
         )
       })}
