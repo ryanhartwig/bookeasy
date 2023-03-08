044d0e353e516c095cc311303f5fa92e4ddcbda1
Ryan1431 <ryan1431@gmail.com>
2023-03-08 13:54:50 -0500

    Use memos storing maps to contain availability, appointmentIndices, services and clients to store related data and populate accordingly within Hours client

--- next/src/app/dashboard/hours.tsx
+++ next/src/app/dashboard/hours.tsx
@@ -4,6 +4,11 @@ import styles from './dashboard.module.scss';
 
 import { sample_base_availability } from '@/utility/sample_data/sample_base_availability';
 import { Appointment } from '@/types/Appointment';
+import { Service } from '@/types/Service';
+import { Client } from '@/types/Client';
+import { useEffect, useMemo } from 'react';
+import { sample_services } from '@/utility/sample_data/sample_services';
+import { sample_clients } from '@/utility/sample_data/sample_clients';
 
 /* 
 
@@ -15,20 +20,37 @@ Formula to convert hours / mins into index:
 
 interface HoursProps {
   day: number,
-  appointments?: Appointment[],
+  appointments: Appointment[],
 }
 
 export const Hours: React.FC<HoursProps> = ({day, appointments}) => {
   const blocks = new Array(96).fill(true);
-  const availability = new Map<number, string[][]>();
 
-  sample_base_availability.slices.forEach(slice => {
-    const {start, end, day} = slice;
-    const current = availability.get(day) || [];
+  const availability = useMemo(() => new Map<number, string[][]>(), []);
+  const appointmentIndices = useMemo(() => new Map<number, Appointment>(), []);
+  const services = useMemo(() => new Map<string, Service>(), []);
+  const clients = useMemo(() => new Map<string, Client>(), []);
 
-    current.push([start, end]);
-    availability.set(day, current);
-  })
+  useEffect(() => {
+    sample_services.forEach(s => services.set(s.id, s));
+    sample_clients.forEach(c => clients.set(c.id, c));
+    
+    appointments.forEach(app => {
+      const date = new Date(app.start_date);
+      const calculatedIndex = (date.getHours() * 4) + (date.getMinutes() / 15);
+      appointmentIndices.set(calculatedIndex, app);
+    });
+
+    sample_base_availability.slices.forEach(slice => {
+      const {start, end, day} = slice;
+      const current = availability.get(day) || [];
+  
+      current.push([start, end]);
+      availability.set(day, current);
+    });
+  }, [appointmentIndices, appointments, availability, clients, services]);
+
+  
 
   return (
     <div className={styles.hours}>
@@ -43,6 +65,7 @@ export const Hours: React.FC<HoursProps> = ({day, appointments}) => {
         const period = hour < 12 ? 'am' : 'pm';
 
         let covered = true;
+
 
         const dayAvailability = availability.get(day);
 
