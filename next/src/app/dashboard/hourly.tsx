c9e748326b0f879e9ccb862501355448c5609bfd
Ryan1431 <ryan1431@gmail.com>
2023-03-08 13:30:12 -0500

    Use getDayRange() to send the right appointments to each Hours component

--- next/src/app/dashboard/hourly.tsx
+++ next/src/app/dashboard/hourly.tsx
@@ -5,8 +5,9 @@ import styles from './dashboard.module.scss';
 import { Hours } from './hours';
 import { useOptimizedResize } from '@/utility/hooks/useOptimizedResize';
 import { getCurrentWeek } from '@/utility/functions/getCurrentWeek';
+import { getDayRange } from '@/utility/functions/getDayRange';
 
-
+import { sample_appointments } from '@/utility/sample_data/sample_appointments';
 
 export const Hourly = () => {
 
@@ -27,9 +28,13 @@ export const Hourly = () => {
     <div className={styles.hourlywrapper} ref={wrapperRef} style={{width: wrapperWidth}}>
       <div className={styles.hourly} ref={hourlyRef} style={{width}}>
         {days.map((_, i) => {
+          const date = new Date(start);
+          date.setDate(date.getDate() + i);
+
+          const [dayStart, dayEnd] = getDayRange(date);
+          const appointments = sample_appointments.filter((app) => dayStart <= app.start_date && dayEnd >= app.start_date);
           
-          
-          return <Hours key={i} day={i} appointments={[]} />
+          return <Hours key={i} day={i} appointments={appointments} />
         })}
       </div>
     </div>
