0caee50f27ae6a48737a0641e3c1396896584642
Ryan1431 <ryan1431@gmail.com>
2023-03-09 16:33:06 -0500

    Move calendar controls to SecondaryHeader component and pass down necessary props

--- next/src/app/calendar/page.tsx
+++ next/src/app/calendar/page.tsx
@@ -1,18 +1,96 @@
+'use client';
+
 import styles from './calendar.module.scss';
 
 
 import { SecondaryHeader } from "@/components/SecondaryHeader"
-import { Calendar } from '@/components/calendar/Calendar';
+import { Calendar, months } from '@/components/calendar/Calendar';
+import { useState, useCallback } from 'react';
+import { getDayRange } from '@/utility/functions/getDayRange';
+import { ReactIconButton } from '@/components/UI/ReactIconButton';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+
+export interface View {        
+  month: number,
+  year: number,
+}
 
 export default function Page() {
+
+  const today = new Date();
+  const initDate = new Date();
+  initDate.setDate(1);
+  initDate.setDate(initDate.getDay() * -1);
+
+  // On selecting day
+  const onSelect = useCallback(([min, max]: [number, number]) => {}, []);
+
+  // First date to show on the top left most cell of the calendar
+  const [startDate, setStartDate] = useState<Date>(initDate);
+
+  // Month currently selected / viewing
+  const [viewing, setViewing] = useState<View>({
+    month: today.getMonth(),
+    year: today.getFullYear(),
+  });
+
+  // Increment / decrement month
+  const onMonthSwitch = useCallback((n: number) => {
+    const newDate = new Date(startDate);
+
+    newDate.setMonth(newDate.getMonth() + n);
+    
+    setViewing({
+      month: newDate.getMonth(),
+      year: newDate.getFullYear(),
+    })
+
+    newDate.setDate(1);
+    newDate.setDate(newDate.getDay() * -1);
+
+    setStartDate(newDate);
+  }, [startDate]);
+
+  const [min, max] = getDayRange(new Date());
+  const reset = useCallback(() => {
+    const td = new Date();
+    const init = new Date();
+    init.setDate(1);
+    init.setDate(init.getDay() * -1);
+    
+    setViewing({
+      month: td.getMonth(),
+      year: td.getFullYear(),
+    });
+    setStartDate(init);
+    onSelect && onSelect([min, max]);
+  }, [max, min, onSelect]);
   
   return (
     <div className={styles.calendar}>
       <SecondaryHeader>
+        {/* Year/Month Select */}
+        <div className='Calendar-period noselect'>
+          <div className='Calendar-period-month'>
+            {/* Month with < / > inc options when showOptions is true */}
 
+            <ReactIconButton buttonSize='30px' onClick={() => onMonthSwitch(0)} style={{borderRadius: '12px'}}>
+              <AiOutlineLeft size={15}/>
+            </ReactIconButton> 
+            <h2>{months[viewing.month]}</h2>
+
+            <ReactIconButton buttonSize='30px' onClick={() => onMonthSwitch(2)} style={{borderRadius: '12px'}}>
+              <AiOutlineRight size={15}/>
+            </ReactIconButton>
+          </div>
+          <div className='Calendar-reset'>
+            <h2>{viewing.year}</h2>
+            <p onClick={reset}>Today</p>
+          </div>
+        </div>
       </SecondaryHeader>
       <div className={styles.content}>
-        <Calendar />
+        <Calendar startDate={startDate} viewing={viewing} />
       </div>
     </div>
   )
--- next/src/components/calendar/Calendar.tsx
+++ next/src/components/calendar/Calendar.tsx
@@ -9,91 +9,23 @@ import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 import { ReactIconButton } from '../UI/ReactIconButton';
 import { useCallback, useState } from 'react';
 import { getDayRange as minMaxDate } from '@/utility/functions/getDayRange';
+import { View } from '@/app/calendar/page';
 
 interface CalendarProps {
-  showOptions?: boolean,
   selected?: [number, number],
   onSelect?: ([min, max]: [number, number]) => any,
+  startDate: Date,
+  viewing: View,
 }
-interface View {        
-  month: number,
-  year: number,
-}
+
 
 export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-export const Calendar = ({showOptions, onSelect, selected}: CalendarProps) => {
-
-  const today = new Date();
-  const initDate = new Date();
-  initDate.setDate(1);
-  initDate.setDate(initDate.getDay() * -1);
-
-  // Month currently selected / viewing
-  const [viewing, setViewing] = useState<View>({
-    month: today.getMonth(),
-    year: today.getFullYear(),
-  });
-
-  // First date to show on the top left most cell of the calendar
-  const [startDate, setStartDate] = useState<Date>(initDate);
-
-  // Increment / decrement month
-  const onMonthSwitch = useCallback((n: number) => {
-    const newDate = new Date(startDate);
-
-    newDate.setMonth(newDate.getMonth() + n);
-    
-    setViewing({
-      month: newDate.getMonth(),
-      year: newDate.getFullYear(),
-    })
-
-    newDate.setDate(1);
-    newDate.setDate(newDate.getDay() * -1);
-
-    setStartDate(newDate);
-  }, [startDate]);
-
-  const [min, max] = minMaxDate(new Date());
-  const reset = useCallback(() => {
-    const td = new Date();
-    const init = new Date();
-    init.setDate(1);
-    init.setDate(init.getDay() * -1);
-    
-    setViewing({
-      month: td.getMonth(),
-      year: td.getFullYear(),
-    });
-    setStartDate(init);
-    onSelect && onSelect([min, max]);
-  }, [max, min, onSelect]);
-
+export const Calendar = ({startDate, onSelect, selected, viewing}: CalendarProps) => {
 
   return (
     <div className='Calendar'>
-      {/* Year/Month Select */}
-      <div className='Calendar-period noselect'>
-        <div className='Calendar-period-month'>
-          {/* Month with < / > inc options when showOptions is true */}
-
-          <ReactIconButton buttonSize='30px' onClick={() => onMonthSwitch(0)} style={{borderRadius: '12px'}}>
-            <AiOutlineLeft size={15}/>
-          </ReactIconButton> 
-          <h2>{months[viewing.month]}</h2>
-
-          <ReactIconButton buttonSize='30px' onClick={() => onMonthSwitch(2)} style={{borderRadius: '12px'}}>
-            <AiOutlineRight size={15}/>
-          </ReactIconButton>
-        </div>
-        <div className='Calendar-reset'>
-          <h2>{viewing.year}</h2>
-          <p onClick={reset}>Today</p>
-        </div>
-      </div>
-
       {/* Day of the Week */}
       <div className='Calendar-days'>
         {days.map(d => <p key={d}>{d.slice(0, 3)}</p>)}
