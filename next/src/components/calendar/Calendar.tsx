15af9fe86b84bb6bfc6a4a1cf47096f3e641e36d
Ryan1431 <ryan1431@gmail.com>
2023-03-10 16:10:08 -0500

    Parse start and end times (getTime()) of current calendar view

--- next/src/components/calendar/Calendar.tsx
+++ next/src/components/calendar/Calendar.tsx
@@ -4,6 +4,11 @@ import './Calendar.css';
 import { Days } from './Days';
 import { View } from '@/app/calendar/page';
 
+import { sample_appointments } from '@/utility/sample_data/sample_appointments';
+import { useState } from 'react';
+import { Appointment } from '@/types/Appointment';
+import { getDayRange } from '@/utility/functions/getDayRange';
+
 interface CalendarProps {
   selected?: [number, number],
   onSelect?: ([min, max]: [number, number]) => any,
@@ -15,6 +20,23 @@ export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fr
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 export const Calendar = ({startDate, onSelect, selected, viewing}: CalendarProps) => {
+
+    
+  const startEnd = new Date(startDate);
+  startEnd.setDate(startEnd.getDate() + 1);
+  const [start] = getDayRange(startEnd);
+
+  startEnd.setDate(startEnd.getDate() + 41);
+  const [_, end] = getDayRange(startEnd);
+
+  console.log(startDate.toDateString());
+  console.log(new Date(start).toDateString());
+  console.log(new Date(end).toDateString());
+
+  // This will be fetched & revalidated on updates, and will only include appointments for the current month
+  const [appointments, setAppointments] = useState<Appointment[]>(sample_appointments.filter(app => {
+    
+  }));
 
   return (
     <div className='Calendar'>
