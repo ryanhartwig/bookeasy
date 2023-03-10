ab202d8f70e6adf23fe31c636e8e051eb25cab57
Ryan1431 <ryan1431@gmail.com>
2023-03-10 12:51:46 -0500

    Import and use clsx to manage Card classes

--- next/src/app/calendar/daily.tsx
+++ next/src/app/calendar/daily.tsx
@@ -1,13 +1,27 @@
+'use client'
+
 import { Card } from '@/components/UI/Card';
+import { Client } from '@/types/Client';
+import { Service } from '@/types/Service';
+import { sample_clients } from '@/utility/sample_data/sample_clients';
+import { sample_services } from '@/utility/sample_data/sample_services';
+import { useMemo, useEffect } from 'react';
 import { Hours } from '../dashboard/hours';
 import styles from './daily.module.scss';
 
 export const Daily = () => {
+  const services = useMemo( () => new Map<string, Service>(), []);
+  const clients = useMemo(() => new Map<string, Client>(), []);
+
+  useEffect(() => {
+    sample_services.forEach(s => services.set(s.id, s));
+    sample_clients.forEach(c => clients.set(c.id, c));
+  }, [clients, services]);
 
   return (
     <div className={styles.daily}>
       <Card className={styles.day_wrapper} >
-        {/* <Hours  /> */}
+        <Hours services={services} clients={clients} appointments={[]} />
       </Card>
     </div>
     
--- next/src/components/UI/Card.tsx
+++ next/src/components/UI/Card.tsx
@@ -1,10 +1,11 @@
 import styles from './card.module.scss';
 
 import { DetailedHTMLProps, HTMLAttributes } from "react";
+import clsx from 'clsx';
 
 export const Card: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = ({children, ...props}) => {
   return (
-    <div {...props} className={styles.card + ' ' + props.className ?? ''}>
+    <div {...props} className={clsx(styles.card, {[`${props.className}`]: !!props.className})}>
       {children}
     </div>
   )
