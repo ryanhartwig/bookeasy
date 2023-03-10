e5395114e5a6f6e1c7297000c4bccceb376cae1f
Ryan1431 <ryan1431@gmail.com>
2023-03-10 13:31:18 -0500

    ForwardRef card to calculate width of scrollbar and hide

--- next/src/app/calendar/daily.tsx
+++ next/src/app/calendar/daily.tsx
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 
 import { Card } from '@/components/UI/Card';
 import { Client } from '@/types/Client';
 import { Service } from '@/types/Service';
 import { sample_clients } from '@/utility/sample_data/sample_clients';
 import { sample_services } from '@/utility/sample_data/sample_services';
-import { useMemo, useEffect } from 'react';
+import { useMemo, useEffect, useState, useRef } from 'react';
 import { Hours } from '../dashboard/hours';
 import styles from './daily.module.scss';
 
@@ -13,14 +13,22 @@ export const Daily = () => {
   const services = useMemo( () => new Map<string, Service>(), []);
   const clients = useMemo(() => new Map<string, Client>(), []);
 
+  const [width, setWidth] = useState<string>('100%');
+  const wrapperRef = useRef<HTMLDivElement>(undefined!);
+
   useEffect(() => {
     sample_services.forEach(s => services.set(s.id, s));
     sample_clients.forEach(c => clients.set(c.id, c));
   }, [clients, services]);
 
+  useEffect(() => {
+    if (!wrapperRef.current ) return;
+    setWidth(`calc(100% + ${wrapperRef.current.offsetWidth - wrapperRef.current.clientWidth}px + 10px)`);
+  }, []);
+  
   return (
     <div className={styles.daily}>
-      <Card className={styles.day_wrapper} >
+      <Card className={styles.day_wrapper} ref={wrapperRef} style={{width, boxShadow: 'none'}} >
         <Hours services={services} clients={clients} appointments={[]} />
       </Card>
     </div>
--- next/src/components/UI/Card.tsx
+++ next/src/components/UI/Card.tsx
@@ -2,11 +2,22 @@ import styles from './card.module.scss';
 
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 import clsx from 'clsx';
+import React from 'react';
 
-export const Card: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = ({children, ...props}) => {
+interface CardProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {};
+
+const Card = React.forwardRef((props: CardProps, ref?: React.ForwardedRef<any>) => {
+  const { children, ...rest } = props;
+  
   return (
-    <div {...props} className={clsx(styles.card, {[`${props.className}`]: !!props.className})}>
+    <div {...rest} ref={ref} className={clsx(styles.card, {[`${rest.className}`]: !!rest.className})}>
       {children}
     </div>
   )
-}
+});
+
+Card.displayName = 'Card';
+
+export { 
+  Card
+};
