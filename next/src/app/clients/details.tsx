7a6457bbaa74c920ebdccc26286020686ce82854
Ryan1431 <ryan1431@gmail.com>
2023-03-13 17:02:28 -0400

    Details cards layout

--- next/src/app/clients/details.tsx
+++ next/src/app/clients/details.tsx
@@ -1,10 +1,23 @@
+import { Card } from '@/components/UI/Card';
 import styles from './clients.module.scss';
 
 export const Details = () => {
 
   return (
     <div className={styles.details}>
-
+      <div>
+        <Card className={styles.card} style={{height: 444}}>
+          
+        </Card>
+        <Card className={styles.card} style={{height: 272}}>
+          
+        </Card>
+      </div>
+      <div>
+        <Card className={styles.card} style={{height: 736}}>
+          
+        </Card>
+      </div>
     </div>
   )
 }
