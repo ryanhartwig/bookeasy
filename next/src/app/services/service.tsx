d6bbc4ca95adde3e4a41025b21fdeddd3754ccb7
Ryan1431 <ryan1431@gmail.com>
2023-03-14 18:03:31 -0400

    Service details

--- next/src/app/services/service.tsx
+++ next/src/app/services/service.tsx
@@ -12,7 +12,11 @@ export const ServiceCard: React.FC<ServiceCardProps> = ({service}) => {
 
   return (
     <div className={styles.service} style={{borderLeftColor: service.color}}>
-
+      <p>{service.name}</p>
+      <p>{service.duration} min</p>
+      <p>{service.provider}</p>
+      <p>a</p>
+      <p>${service.cost.toFixed(2)}</p>
     </div>
   )
 }
