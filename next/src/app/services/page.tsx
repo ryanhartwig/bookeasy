2eb55a554ca2d2510dc54e65540bcb91b28ce093
Ryan1431 <ryan1431@gmail.com>
2023-03-14 17:38:59 -0400

    Setup services page skeleton and styling

--- next/src/app/services/page.tsx
+++ next/src/app/services/page.tsx
@@ -1,5 +1,17 @@
+import { SectionLabel } from '@/components/UI/SectionLabel';
+import styles from './services.module.scss';
+
 export default function Page() {
+
+  
   return (
-    <p>services</p>
+    <div className={styles.services}>
+      <div className={styles.labels}>
+        {['Name', 'Duration', 'Provider', 'Assignee(s)', 'Cost'].map((t) => 
+          <p key={t}>{t}</p>
+        )}
+      </div>
+      <SectionLabel label='My Services' />
+    </div>
   )
 }
