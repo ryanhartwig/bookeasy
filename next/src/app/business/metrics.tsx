96a7a3450d7f1a00d0b55b6dfaf66f6c694db268
Ryan1431 <ryan1431@gmail.com>
2023-03-15 17:44:43 -0400

    Disable / hide increment / decrement buttons to prevent out of range year for data

--- next/src/app/business/metrics.tsx
+++ next/src/app/business/metrics.tsx
@@ -2,7 +2,8 @@
 
 import { ReactIconButton } from "@/components/UI/ReactIconButton";
 import { User } from "@/types/User";
-import { useCallback, useRef, useState } from "react";
+import clsx from "clsx";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import styles from './business.module.scss';
 
@@ -11,6 +12,7 @@ interface MetricsProps {
 }
 
 export const Metrics: React.FC<MetricsProps> = ({user}) => {
+  const today = new Date();
 
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());  
 
@@ -22,20 +24,37 @@ export const Metrics: React.FC<MetricsProps> = ({user}) => {
       return date;
     });
   }, []);
+
+  const year_joined = useMemo<number>(() => {
+    const date = new Date(user.created);
+    return date.getFullYear();
+  }, [user.created]);
   
 
   return (
     <>
       <div className={styles.year_select}>
-        <ReactIconButton buttonSize='30px' onClick={() => onYearChange(-1)} style={{borderRadius: '12px'}}>
+        <ReactIconButton 
+          className={clsx({[styles.disabled]: year_joined === selectedDate.getFullYear()})} 
+          buttonSize='30px' 
+          active={false}
+          onClick={() => onYearChange(-1)} 
+          style={{borderRadius: '12px'}}
+        >
           <AiOutlineLeft size={15}/>
-        </ReactIconButton> 
+        </ReactIconButton>
         <h2>{selectedDate.getFullYear()}</h2>
-
-        <ReactIconButton buttonSize='30px' onClick={() => onYearChange(1)} style={{borderRadius: '12px'}}>
+        <ReactIconButton 
+          className={clsx({[styles.disabled]: selectedDate.getFullYear() === today.getFullYear()})}
+          buttonSize='30px' 
+          onClick={() => onYearChange(1)} 
+          style={{borderRadius: '12px'}}
+        >
           <AiOutlineRight size={15}/>
         </ReactIconButton>
       </div>
+
+      
     </>
   )
 }
