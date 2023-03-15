04980bb9cd8e4aae20f5602402c90ee223eb7280
Ryan1431 <ryan1431@gmail.com>
2023-03-15 09:55:32 -0400

    Card layout / skeleton

--- next/src/app/business/page.tsx
+++ next/src/app/business/page.tsx
@@ -1,5 +1,20 @@
+import { Card } from '@/components/UI/Card';
+import clsx from 'clsx';
+import styles from './business.module.scss';
+
 export default function Page() {
   return (
-    <p>my business</p>
+    <div className={styles.business}>
+      {/* Left panels */}
+      <div>
+        <Card className={clsx(styles.card, styles.overview)} />
+        <Card className={clsx(styles.card, styles.metrics)} />
+      </div>
+
+      {/* Right panel */}
+      <div>
+        <Card className={clsx(styles.card, styles.prefs)} />
+      </div>
+    </div>
   )
 }
