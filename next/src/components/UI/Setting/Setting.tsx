48a6164787cd7a0765a9ecdd70f777ab25a50678
Ryan1431 <ryan1431@gmail.com>
2023-03-16 12:25:11 -0400

    Setup Settings with Edit button

--- next/src/components/UI/Setting/Setting.tsx
+++ next/src/components/UI/Setting/Setting.tsx
@@ -4,14 +4,24 @@ import styles from './setting.module.scss';
 interface SettingProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   label: string,
   children?: JSX.Element | never[],
-  toggle?: boolean,
+  /**
+   * If provided, will use switch instead of 'Edit' button
+   */
+  toggleState?: boolean,
+  onAction?: (...args: any) => any,
 }
 
-export const Setting: React.FC<SettingProps> = ({label, children, toggle = false}) => {
+export const Setting: React.FC<SettingProps> = ({label, children, toggleState, onAction}) => {
 
   return (
     <div className={styles.Setting}>
-      {children}
+      <p className={styles.label}>{label}</p>
+      <div className={styles.value}>
+        {children}
+      </div>
+      <div className={styles.action} onClick={onAction}>
+        {toggleState === undefined && <p>Edit</p>}
+      </div>
     </div>
   )
-}
+} 
