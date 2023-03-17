d3c51a423c48187580e312b1e5b4fef93ed613db
Ryan1431 <ryan1431@gmail.com>
2023-03-17 19:29:54 -0400

    Render setting component for each day and prepare for availability data

--- next/src/app/business/tabs/Availability.tsx
+++ next/src/app/business/tabs/Availability.tsx
@@ -1,12 +1,22 @@
+'use client';
+
+import { Setting } from '@/components/UI/Setting/Setting';
 import styles from './tabs.module.scss';
 
 export const Availability = () => {
 
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  
   return (
     <div className={styles.Availability}>
       <div className={styles.header}>
         <p>Bookable Hours</p>
       </div>
+      {days.map(d => (
+        <Setting label={d} key={d}>
+          <p>n</p>
+        </Setting>
+      ))}
     </div>
   )
 }
