6d25d1de701c3ea2b185960d54e57e9391d1e65b
Ryan1431 <ryan1431@gmail.com>
2023-03-20 17:33:28 -0400

    Move Overview into it's own component and pass necessary data down

--- next/src/app/teams/page.tsx
+++ next/src/app/teams/page.tsx
@@ -5,9 +5,7 @@ import { Business } from '@/types/Business';
 import { useMemo, useState } from 'react';
 import { Teams } from './teams';
 import styles from './teams.module.scss';
-import Image from 'next/image';
 
-import teamDefault from '../../../public/assets/team_default.png';
 import clsx from 'clsx';
 import { User } from '@/types/User';
 import { sample_members } from '@/utility/sample_data/sample_user';
@@ -15,6 +13,7 @@ import { Client } from '@/types/Client';
 import { sample_clients } from '@/utility/sample_data/sample_clients';
 import { Service } from '@/types/Service';
 import { sample_services } from '@/utility/sample_data/sample_services';
+import { Overview } from './overview';
 
 
 export default function Page() {
@@ -43,37 +42,21 @@ export default function Page() {
 
       <div className={styles.team_overview}>
         {selected ? (
-        <>
-          <div className={styles.left}>
-            <Card className={clsx(styles.card, styles.overview)}>
-              <Image src={teamDefault} width={139} alt='Add team icon' />
-              <h2>{selected.name}</h2>
-              <hr />
-              <div>
-                <div>
-                  <p>{members.length}</p>
-                  <p>member(s)</p>
-                </div>
-                <div>
-                  <p>{clients.length}</p>
-                  <p>client(s)</p>
-                </div>
-                <div>
-                  <p>{services.length}</p>
-                  <p>service(s)</p>
-                </div>
-              </div>
-            </Card>
-            <Card className={styles.card}>
+          <>
+            <div className={styles.left}>
+              <Card className={clsx(styles.card, styles.overview)}>
+                <Overview selected={selected} members={members} clients={clients} services={services} />
+              </Card>
+              <Card className={styles.card}>
 
-            </Card>
-          </div>
-          <div className={styles.right}>
-            <Card className={styles.card}>
-              
-            </Card>
-          </div>
-        </> 
+              </Card>
+            </div>
+            <div className={styles.right}>
+              <Card className={styles.card}>
+                
+              </Card>
+            </div>
+          </> 
         ) : <p className={styles.select}>Select a team to see details</p>}
       </div>
       
