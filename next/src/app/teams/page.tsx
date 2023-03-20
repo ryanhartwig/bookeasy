f1a592833dbcec393928d08d1d2d4a3eafe345e3
Ryan1431 <ryan1431@gmail.com>
2023-03-20 17:29:58 -0400

    Parse sample data to show team overview statistics

--- next/src/app/teams/page.tsx
+++ next/src/app/teams/page.tsx
@@ -2,18 +2,37 @@
 
 import { Card } from '@/components/UI/Card/Card';
 import { Business } from '@/types/Business';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Teams } from './teams';
 import styles from './teams.module.scss';
 import Image from 'next/image';
 
 import teamDefault from '../../../public/assets/team_default.png';
 import clsx from 'clsx';
+import { User } from '@/types/User';
+import { sample_members } from '@/utility/sample_data/sample_user';
+import { Client } from '@/types/Client';
+import { sample_clients } from '@/utility/sample_data/sample_clients';
+import { Service } from '@/types/Service';
+import { sample_services } from '@/utility/sample_data/sample_services';
 
 
 export default function Page() {
   
   const [selected, setSelected] = useState<Business>();
+
+  const members = useMemo<User[]>(() => selected 
+    ? sample_members.filter(m => m.business_ids.includes(selected.id)) 
+    : []
+  , [selected]);
+  const clients = useMemo<Client[]>(() => selected 
+    ? sample_clients.filter(c => c.business_id === selected.id) 
+    : []
+  , [selected]);
+  const services = useMemo<Service[]>(() => selected
+    ? sample_services.filter(s => s.business_id === selected.id)
+    : []
+  , [selected]);
   
   return (
     <div className={styles.Teams}>
@@ -32,15 +51,15 @@ export default function Page() {
               <hr />
               <div>
                 <div>
-                  <p>n</p>
+                  <p>{members.length}</p>
                   <p>member(s)</p>
                 </div>
                 <div>
-                  <p>n</p>
+                  <p>{clients.length}</p>
                   <p>client(s)</p>
                 </div>
                 <div>
-                  <p>n</p>
+                  <p>{services.length}</p>
                   <p>service(s)</p>
                 </div>
               </div>
