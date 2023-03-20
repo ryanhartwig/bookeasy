84b760986ce863c3be298ad7d7eec387f8d10fa3
Ryan1431 <ryan1431@gmail.com>
2023-03-20 08:27:02 -0400

    Preprocess and show start-end slices in availability tab

--- next/src/app/business/tabs/Availability.tsx
+++ next/src/app/business/tabs/Availability.tsx
@@ -1,10 +1,20 @@
 'use client';
 
 import { Setting } from '@/components/UI/Setting/Setting';
+import { sample_base_availability } from '@/utility/sample_data/sample_base_availability';
+import { useEffect, useMemo } from 'react';
 import styles from './tabs.module.scss';
 
 export const Availability = () => {
 
+  const map = useMemo(() => new Map<number, {start: string, end: string}[]>(), []);
+
+  useEffect(() => {
+    sample_base_availability.slices.forEach(slice => {
+      map.set(slice.day, (map.get(slice.day) ?? []).concat([{ start: slice.start, end: slice.end }]));
+    });
+  }, [map]);
+  
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   
   return (
@@ -12,11 +22,16 @@ export const Availability = () => {
       <div className={styles.header}>
         <p>Bookable Hours</p>
       </div>
-      {days.map(d => (
+      {days.map((d, i) => (
         <Setting label={d} key={d}>
-          <p>n</p>
+          {(map.get(i) ?? []).map(({start, end}: {start: string, end: string}) => {
+            return <div key={start}>
+              <p>{start} - {end}</p>
+            </div>
+          })}
         </Setting>
       ))}
     </div>
   )
 }
+ 
