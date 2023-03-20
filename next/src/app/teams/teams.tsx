d6dbbaf87a7b7b4678919f7faa2a878f4623657e
Ryan1431 <ryan1431@gmail.com>
2023-03-20 13:27:50 -0400

    Add create team card

--- next/src/app/teams/teams.tsx
+++ next/src/app/teams/teams.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import Image from 'next/image';
 import styles from './teams.module.scss';
+
+import addTeam from '../../../public/assets/team_add.svg'
 
 export const Teams = () => {
 
@@ -13,7 +16,7 @@ export const Teams = () => {
       {/* Create team */}
       <div className={styles.create_team}>
         <p>Create Team</p>
-        
+        <Image src={addTeam} alt='Add team icon' />
       </div>
     </div>
   )
