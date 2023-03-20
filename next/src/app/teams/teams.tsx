296bd8e66ec4cf741d1f0022734911a0b92db1f5
Ryan1431 <ryan1431@gmail.com>
2023-03-20 13:50:41 -0400

    Add selected state and styling for teams

--- next/src/app/teams/teams.tsx
+++ next/src/app/teams/teams.tsx
@@ -4,14 +4,30 @@ import Image from 'next/image';
 import styles from './teams.module.scss';
 
 import addTeam from '../../../public/assets/team_add.svg'
+import { useMemo, useState } from 'react';
+import { sample_businesses } from '@/utility/sample_data/sample_businesses';
+import { sample_user } from '@/utility/sample_data/sample_user';
+import { Card } from '@/components/UI/Card/Card';
+import clsx from 'clsx';
+
+
 
 export const Teams = () => {
-
+  
+  const teams = useMemo(() => sample_businesses.filter(b => b.id !== sample_user.own_business_id), []);
+  const [selected, setSelected] = useState<string>('');
 
   return (
     <div className={styles.user_teams}>
       {/* Teams user belongs to */}
+      {teams.map(t => (
+        <Card key={t.id} 
+          className={clsx(styles.team, {[styles.selected]: selected === t.id})}
+          onClick={() => setSelected(t.id)}
+        >
 
+        </Card>
+      ))}
 
       {/* Create team */}
       <div className={styles.create_team}>
