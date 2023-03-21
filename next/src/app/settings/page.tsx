eb4c5418153d554a04fa06152c46864bea2a3ca9
Ryan1431 <ryan1431@gmail.com>
2023-03-21 12:39:05 -0400

    Adjust sectionlabel css locally in settings page & implement

--- next/src/app/settings/page.tsx
+++ next/src/app/settings/page.tsx
@@ -1,5 +1,15 @@
+import { Card } from '@/components/UI/Card/Card';
+import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
+import styles from './settings.module.scss';
+
 export default function Page() {
   return (
-    <p>settings</p>
+    <div className={styles.Settings}>
+      <Card className={styles.card}>
+        <SectionLabel label="Profile Settings" className={styles.label} />
+        <SectionLabel label="Privacy Settings" className={styles.label} />
+        <SectionLabel label="Notification Settings" className={styles.label} />
+      </Card>
+    </div>
   )
 }
--- next/src/components/UI/SectionLabel/SectionLabel.tsx
+++ next/src/components/UI/SectionLabel/SectionLabel.tsx
@@ -1,6 +1,7 @@
 import styles from './sectionlabel.module.scss';
 
 import { DetailedHTMLProps, HTMLAttributes } from "react";
+import clsx from 'clsx';
 
 interface SectionLabelProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   label: string,
@@ -9,7 +10,7 @@ interface SectionLabelProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElem
 export const SectionLabel: React.FC<SectionLabelProps> = ({label, ...props}) => {
 
   return (
-    <div {...props} className={styles.section_label}>
+    <div {...props} className={clsx(styles.section_label, props.className || '')}>
       <p>{label}</p>
       <hr />
     </div>
