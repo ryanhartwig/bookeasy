5e60d710d595a6567a35281a9cc6571f1616b7d8
Ryan1431 <ryan1431@gmail.com>
2023-03-21 09:52:29 -0400

    Update staff data to show proper data and add Details button

--- next/src/components/business_settings/Staff.tsx
+++ next/src/components/business_settings/Staff.tsx
@@ -6,6 +6,7 @@ import { User } from "@/types/User";
 
 import styles from './tabs.module.scss';
 import Image from 'next/image';
+import { useMemo } from "react";
 
 interface StaffProps {
   members: User[],
@@ -15,26 +16,30 @@ interface StaffProps {
 
 export const Staff: React.FC<StaffProps> = ({members, services, clients}) => {
   
-  
+  const staff = useMemo(() => 
+    members.map(m => (
+      <div className={styles.client} key={m.id}>
+        <div>
+          <Image src={m.avatar || ''} alt="Member avatar" height={28} />
+        </div>
+        <div>
+          <p>{m.name}</p>
+          <p>{services.filter(s => s.user_ids.includes(m.id)).length}</p>
+          <p>{clients.filter(c => c.assigned_staff.includes(m.id)).length}</p>
+          <p>{new Date(m.created).toDateString().split(' ').slice(1).join(' ')}</p>
+          <p className={styles.details}>Details</p>
+        </div>
+      </div> 
+      )
+    )
+  , [clients, members, services]);
 
   return (
     <div className={styles.ClientList}>
       <div className={styles.header}>
-        {['Name', 'Services', 'Clients', 'Date Added'].map(t => <p key={t}>{t}</p>)}
+        {['Name', 'Services', 'Clients', 'Date Added', ''].map(t => <p key={t}>{t}</p>)}
       </div>
-      {members.map(m => (
-        <div className={styles.client} key={m.id}>
-          <div>
-            <Image src={m.avatar || ''} alt="Member avatar" height={28} />
-          </div>
-          <div>
-            <p>{m.name}</p>
-            <p>{services.filter(s => s.user_ids.includes(m.id)).length}</p>
-            <p></p>
-            <p>{new Date(m.created).toDateString().split(' ').slice(1).join(' ')}</p>
-          </div>
-        </div>
-      ))}
+      {staff}
     </div>
   )
 }
