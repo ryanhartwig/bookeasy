2eba9f31d4321afb89b3eadffe516fc776ab8e89
Ryan1431 <ryan1431@gmail.com>
2023-03-21 12:43:19 -0400

    Add Profile Settings

--- next/src/app/settings/page.tsx
+++ next/src/app/settings/page.tsx
@@ -1,12 +1,32 @@
 import { Card } from '@/components/UI/Card/Card';
 import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
+import { Setting } from '@/components/UI/Setting/Setting';
+import { sample_user } from '@/utility/sample_data/sample_user';
+import Image from 'next/image';
 import styles from './settings.module.scss';
 
 export default function Page() {
+
+  const user = sample_user;
+  
   return (
     <div className={styles.Settings}>
       <Card className={styles.card}>
         <SectionLabel label="Profile Settings" className={styles.label} />
+          <div className={styles.setting_block}>
+            <Setting label='Picture'>
+              <Image src={user.avatar ?? ''} alt='User avatar' width={60} />
+            </Setting>
+            <Setting label='Name'>
+              <p>{user.name}</p>
+            </Setting>
+            <Setting label='Email'>
+              <p>{user.email}</p>
+            </Setting>
+            <Setting label='Phone' style={{borderBottom: 'none'}}>
+              <p>{user.phone ?? 'None'}</p>
+            </Setting>
+          </div>
         <SectionLabel label="Privacy Settings" className={styles.label} />
         <SectionLabel label="Notification Settings" className={styles.label} />
       </Card>
