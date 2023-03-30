ea6175cf217e5686e058387ec703c23a28e726b5
Ryan1431 <ryan1431@gmail.com>
2023-03-30 13:03:53 -0400

    Implement getAllServices fetch in dashboard, clean up unused imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,76 +10,15 @@ import { Card } from '@/components/UI/Card/Card';
 import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
 import { Header } from '@/components/Header';
 
-import { gql } from '@apollo/client';
-import { getClient } from '@/utility/functions/getClient';
-import { Appointment } from '@/types/Appointment';
-import { Service } from '@/types/Service';
 import { getAllAppointments } from '@/utility/functions/fetch/getAllAppointments';
-
-
-
-const servicesQuery = gql`
-  query UserServicesQuery($after: String!,  $id: ID!) {
-  user(by: {id: $id}) {
-    services(first:100, after: $after) {
-      edges {
-        node {
-          id 
-          business { id }
-          name
-          duration
-          provider
-          cost
-          isVideo
-          color
-        }
-      }
-      pageInfo {
-        hasNextPage
-        endCursor
-      }
-    }
-  }
-}
-`
+import { getAllServices } from '@/utility/functions/fetch/getAllServices';
 
 export default async function Page() {
+  const { appointments } = await getAllAppointments('user_01GWHJK2PJ3C1DGYJY32YSJFQ3');
+  const { services } = await getAllServices('user_01GWHJK2PJ3C1DGYJY32YSJFQ3');
 
-  const { appointments, error } = await getAllAppointments('user_01GWHJK2PJ3C1DGYJY32YSJFQ3');
-  const allServices: Service[] = [];
-
-  const client = getClient();
-  let servicesAfter = '';
-
-  const fillServices = async () => {
-    try {
-      const { data } = await client.query({ query: servicesQuery, variables: {
-        after: servicesAfter,
-        id: "user_01GWHJK2PJ3C1DGYJY32YSJFQ3" // default sample user
-      } });
-      const { services } = data.user;
-
-      allServices.push(...services.edges.map((edge: any) => parseToService(edge.node)))
-      if (services.pageInfo.hasNextPage) {
-        servicesAfter = services.pageInfo.endCursor;
-        await fillServices();
-      }
-    } catch(e) {
-      console.log(e);
-    }
-  }
-
-
-  
-  await fillServices();
-
-  console.log(appointments);
-  console.log(allServices);
-
-  
   const [start, end] = getCurrentWeek();
 
-  
   return (
     <>
       <Header text='Dashboard' />
@@ -104,7 +43,7 @@ export default async function Page() {
         </SecondaryHeader>
         <div className={styles.content}>
           <SectionLabel label='Today' />
-          <Appointments appointments={appointments} services={allServices} />
+          <Appointments appointments={appointments} services={services} />
           <SectionLabel label='This Week' />
           <Card className={styles.weekview_card}>
             <WeekDayNames start={start} />
