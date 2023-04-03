a6688710a6399bfcaec7cd0ee0e400b69c4b0ea1
Ryan1431 <ryan1431@gmail.com>
2023-04-03 13:53:46 -0400

    Add default avatar to clients sidebar

--- src/app/clients/clients.tsx
+++ src/app/clients/clients.tsx
@@ -6,6 +6,9 @@ import clsx from 'clsx';
 import { Client } from '@/types/Client';
 import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
 import { Business } from '@/types/Business';
+import { sample_clients } from '@/utility/sample_data/sample_clients';
+
+import { IoPerson, IoPersonCircleSharp } from 'react-icons/io5';
 
 interface ClientsProps {
   selected: Client | undefined,
@@ -27,6 +30,8 @@ export const Clients: React.FC<ClientsProps> = ({selected, setSelected, clients,
           // space separated lazy search
           .filter(c => !query.length || query.split(' ').every(slice => c.name.toLowerCase().includes(slice.toLowerCase())))
         ;
+
+        console.log('avatar: ', sample_clients[0].avatar);
         
         return (
           <div key={b.id} className={styles.client_team}>
@@ -39,7 +44,8 @@ export const Clients: React.FC<ClientsProps> = ({selected, setSelected, clients,
                   setSelected(c);
                 }}
               >
-                {c.avatar && <Image alt="client avatar" src={c.avatar} style={{width: 30, height: 30}} />}
+                {c.avatar ? <Image alt="client avatar" src={c.avatar} style={{width: 30, height: 30}} />
+                : <IoPersonCircleSharp fontSize={30} color={'grey'} />}
                 <p>{c.name}</p>
               </div>
             ))}
