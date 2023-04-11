ff48938d8ea4ad826eb07a0a94941812438f1a89
Ryan1431 <ryan1431@gmail.com>
2023-04-11 18:37:45 -0400

    Import down arrow and position

--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -1,13 +1,18 @@
 import styles from './select.module.scss';
 
+import { BsChevronDown } from 'react-icons/bs';
+
 interface SelectProps {
-  list: JSX.Element[]
+  list: JSX.Element[],
+  placeholder: string,
 }
 
 export const Select: React.FC<SelectProps> = ({list}) => {
   return (
     <div className={styles.input}>
-      
+      <div className={styles.down}>
+        <BsChevronDown />
+      </div>
     </div>
   )
 }
