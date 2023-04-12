4d44921e0be6f55d37fa0c40df831645bbbbe116
Ryan1431 <ryan1431@gmail.com>
2023-04-12 18:29:07 -0400

    Add options state and dropdown div

--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -1,6 +1,7 @@
 import styles from './select.module.scss';
 
 import { BsChevronDown } from 'react-icons/bs';
+import { useState } from 'react';
 
 interface SelectProps {
   list: JSX.Element[],
@@ -8,12 +9,19 @@ interface SelectProps {
 }
 
 export const Select: React.FC<SelectProps> = ({list, placeholder = ""}) => {
+
+  const [optionsShowing, setOptionsShowing] = useState<boolean>(false);
+  
   return (
     <div className={styles.input}>
       <p>{placeholder}</p>
       <div className={styles.down}>
         <BsChevronDown />
       </div>
+      
+      {optionsShowing && <div className={styles.options}>
+        
+      </div>}
     </div>
   )
 }
