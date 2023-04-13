717b195a8ab4f09ce3e34bb658d376554e767856
Ryan1431 <ryan1431@gmail.com>
2023-04-13 08:23:27 -0400

    Handle open and closed state for select dropdowns, implementing useClickout

--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -1,7 +1,8 @@
 import styles from './select.module.scss';
 
 import { BsChevronDown } from 'react-icons/bs';
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
+import { useClickout } from '@/utility/hooks/useClickout';
 
 interface SelectProps {
   list: JSX.Element[],
@@ -11,17 +12,29 @@ interface SelectProps {
 export const Select: React.FC<SelectProps> = ({list, placeholder = ""}) => {
 
   const [optionsShowing, setOptionsShowing] = useState<boolean>(false);
+  const selectRef = useRef<HTMLDivElement>(undefined!);
+  const optionsRef = useRef<HTMLDivElement>(undefined!);
+
+  useClickout(() => setOptionsShowing(false), optionsShowing, selectRef);
+
+  const showOptions = useCallback((e: any) => {
+    if (!optionsRef.current) return setOptionsShowing(true);
+    if (optionsRef.current.contains(e.target)) return;
+    setOptionsShowing(p => !p);
+  }, []);
   
   return (
-    <div className={styles.input}>
+    <div className={styles.input} ref={selectRef} onClick={showOptions} >
       <p>{placeholder}</p>
       <div className={styles.down}>
         <BsChevronDown />
       </div>
       
-      {optionsShowing && <div className={styles.options}>
+      {optionsShowing && 
+        <div className={styles.options} ref={optionsRef}>
         
-      </div>}
+        </div>
+      }
     </div>
   )
 }
