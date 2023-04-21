bc5fb201e06759f97905d8d2d1913cebf80ebf42
Ryan1431 <ryan1431@gmail.com>
2023-04-21 18:04:38 -0400

    Create AppointmentData type for GQL query response to include all necessary data

--- src/types/Appointment.ts
+++ src/types/Appointment.ts
@@ -11,3 +11,27 @@ export interface Appointment {
   isVideo: boolean,
   isPaid: boolean,
 };
+
+/**
+ * Includes all necessary fields to fill appointment cards
+ */
+export interface AppointmentData {
+  id: string,
+  service: {
+    name: string,
+    duration: number,
+    color: string,
+  }
+  business: {
+    name: string
+  }
+  client: {
+    name: string,
+  }
+  start_date: string,
+  end_date: string,
+  service_duration: number,
+  service_cost: number,
+  is_video: boolean,
+  is_paid: boolean
+}
