612233cae568350019cad31f736b7e5d3e385581
Ryan1431 <ryan1431@gmail.com>
2023-04-22 09:10:58 -0400

    Add GET_USER_APPOINTMENTS query

--- src/utility/queries/appointmentQueries.ts
+++ src/utility/queries/appointmentQueries.ts
@@ -1,2 +1,26 @@
 import { gql } from '@apollo/client';
 
+export const GET_USER_APPOINTMENTS = gql`
+  query($userId: ID!, $rangeStart: String, $rangeEnd: String) {
+    getUserAppointments(user_id: $userId, range_start: $rangeStart, range_end: $rangeEnd) {
+      id
+      service {
+        name
+        duration
+        color
+      }
+      business {
+        name
+      }
+      client {
+        name
+      }
+      start_date
+      end_date
+      service_duration
+      service_cost
+      is_video
+      is_paid
+    }
+  }
+`
