647fccd16ae869b9fd29dbc7cbd70cb51bcff392
Ryan1431 <ryan1431@gmail.com>
2023-04-26 12:56:01 -0400

    Add paid / unpaid status on booked / previous client appointments

--- src/app/clients/appointment.tsx
+++ src/app/clients/appointment.tsx
@@ -13,15 +13,18 @@ export const AppointmentCard: React.FC<AppointmentCardProps> = ({app}) => {
 
   return (
     <div className={styles.app} style={{borderLeftColor: app.service.color}}>
+      <p className={styles.paid} style={{color: !app.is_paid ? '' : 'rgb(39, 131, 174)'}} >{app.is_paid ? '✓ Paid' : 'Unpaid'}</p>
+      
       <div>
         <p>{date}</p>
         <p>{app.service.duration} min</p>
       </div>
-      
+
       <div>
         <p>{app.service.name}</p>
         <p>See in calendar</p>
       </div>
+
     </div>
   )
 }
