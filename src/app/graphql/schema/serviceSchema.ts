39f6c0baa2951deaf46ee1b94b7d98b98326a8a5
Ryan1431 <ryan1431@gmail.com>
2023-05-02 07:51:12 -0400

    Edit service resolver and typedef

--- src/app/graphql/schema/serviceSchema.ts
+++ src/app/graphql/schema/serviceSchema.ts
@@ -6,19 +6,34 @@ export const serviceResolvers = {
     addService: async (parent: any, args: any) => {
       const { id, name, duration, provider, cost, is_video, color, deleted, business_id, assigned_users } = args.service;
 
-      const addServiceResponse = await db.query(`insert into service values (
+      await db.query(`insert into service values (
         $1, $2, $3, $4, $5, $6, $7, $8, $9
       ) returning *`, [id, business_id, name, duration, provider, cost, is_video, color, deleted]);
 
-      if (!addServiceResponse.rowCount) throwGQLError('Error inserting service with id: ' + id);
-
       for (const user_id of assigned_users) {
-        const response = await db.query('insert into users_services values ($1, $2) returning *', [id, user_id]);
-        if (!response.rowCount) throwGQLError('Could not create relationships for user with id: ' + user_id)
+        await db.query('insert into users_services values ($1, $2) returning *', [id, user_id]);
       }
 
       return id;
     },
+    editService: async (parent: any, args: any) => {
+      const { id, name, cost, duration, color, is_video, assigned_users } = args.service;
+
+      await db.query(`update service set
+        name = $2,
+        cost = $3, 
+        duration = $4, 
+        is_video = $5,
+        color = $6
+      where id = $1`, [id, name, cost, duration, is_video, color]);
+      
+      await db.query('delete from users_services where service_id = $1', [id]);
+      for (const user_id of assigned_users) {
+        await db.query('insert into users_services values ($1, $2)', [id, user_id]);
+      }
+
+      return id;
+    }
   },
 }
 
@@ -37,17 +52,7 @@ export const serviceTypeDefs = `#graphql
   }
 
   type Mutation {
-    addService(service: ServiceInput): String!
+    addService(service: ServiceInput): String!,
+    editService(service: ServiceInput): String!,
   }
 `;
-
-// id: string,
-// name: string,
-// duration: number,
-// provider: string,
-// cost: number,
-// is_video: boolean,
-// color: string,
-// deleted: boolean,
-// assigned_users: string[],
-// business_id: string,
