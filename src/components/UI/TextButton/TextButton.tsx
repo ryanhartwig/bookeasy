ced8be0ac79581d4651ab288a53ff901587f56da
Ryan1431 <ryan1431@gmail.com>
2023-05-17 09:25:32 -0400

    Use altColor property in TextButton component for theme selection

--- src/components/UI/TextButton/TextButton.tsx
+++ src/components/UI/TextButton/TextButton.tsx
@@ -4,13 +4,22 @@ import styles from './textbutton.module.scss';
 
 interface TextButtonProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   children: React.ReactNode,
+  altColor?: boolean,
 }
 
-export const TextButton: React.FC<TextButtonProps> = ({children, ...props}) => {
+export const TextButton: React.FC<TextButtonProps> = ({children, altColor = false, ...props}) => {
+
+  const color = altColor
+    ? '#ff462d'
+    : '#216FDB';
+
+  const backgroundColor = altColor
+    ? '#ff462d19'
+    : '#216fdb19'
 
   return (
     <div {...props} className={clsx(styles.action, props.className || '')}>
-      <p>{children}</p>
+      <p style={{color, backgroundColor}} >{children}</p>
     </div>
   )
 }
