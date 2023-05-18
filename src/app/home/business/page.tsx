01dc67ea12eae64be75eebf280dbf8bc347fcef7
Ryan1431 <ryan1431@gmail.com>
2023-05-18 18:09:10 -0400

    Use session to retrieve user in business view

--- src/app/home/business/page.tsx
+++ src/app/home/business/page.tsx
@@ -1,21 +1,23 @@
 'use client';
 
 import { Header } from '@/components/Header';
+import { User } from '@/types/User';
 import { GET_BUSINESS_WITH_STAFF_ID } from '@/utility/queries/businessQueries';
-import { GET_USER } from '@/utility/queries/userQueries';
-import { userId } from '@/utility/sample_data/sample_userId';
 import { useQuery } from '@apollo/client';
+import { useSession } from 'next-auth/react';
 import { Business } from './business';
 
 export default function Page() {
 
-  const { data: userData } = useQuery(GET_USER, { variables: { userId } });
-  const { data: businessData } = useQuery(GET_BUSINESS_WITH_STAFF_ID, { variables: { businessId: userData?.getUser?.own_business_id }, skip: !userData?.getUser});
+  const { data: session } = useSession();
+  const user = session?.user as User | undefined;
+
+  const { data: businessData } = useQuery(GET_BUSINESS_WITH_STAFF_ID, { variables: { businessId: user?.own_business_id }, skip: !user});
 
   return (
     <>
       <Header text='My Business' />
-      {userData && businessData && <Business business={businessData.getBusiness} user={userData.getUser} staffId={businessData.getBusiness.staff[0].id} />}
+      {user && businessData && <Business business={businessData.getBusiness} user={user} staffId={businessData.getBusiness.staff[0].id} />}
     </>
   );
 }
