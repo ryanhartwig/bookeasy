cf278b39e535a1abe47e0bf76a223cf36af9c5f4
Ryan1431 <ryan1431@gmail.com>
2023-05-19 12:50:57 -0400

    Move classname property in Input UI component from input to wrapping div

--- src/app/login/register/page.tsx
+++ src/app/login/register/page.tsx
@@ -7,9 +7,10 @@ export default function Page() {
   return (
     <div className={styles.form}>
       <p>Create a new account</p>
-      <Input />
-      <Input />
-      <Input />
+      <Input className={styles.input} placeholder="Your name" />
+      <Input className={styles.input} placeholder="Your email" />
+      <Input className={styles.input} placeholder="Your password" />
+      <Input className={styles.input} placeholder="Confirm password" />
     </div>
   )
 } 
--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -10,13 +10,13 @@ interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 export const Input: React.FC<InputProps> = ({errorMessage, ...props}) => {
 
   return (
-    <div className={clsx(styles.inputWrapper, {[styles.checkbox]: props.type === 'checkbox'})}>
+    <div className={clsx(styles.inputWrapper, props.className || '', {[styles.checkbox]: props.type === 'checkbox'})}>
       {errorMessage && <div className={styles.errorMessage}>
         <BiErrorCircle fontSize={14} style={{marginRight: 8}} />
         <p>{errorMessage}</p>
       </div>}
       {props.required && !props.value && <p className={styles.required}>*</p>}
-      <input {...props} className={clsx(styles.input, props.className || '', {[styles.invalid]: !!errorMessage})} />
+      <input {...props} className={clsx(styles.input, {[styles.invalid]: !!errorMessage})} />
     </div>
   )
 }
