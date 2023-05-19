8c365165789c727755c75bd2ff220bf31ba00012
Ryan1431 <ryan1431@gmail.com>
2023-05-19 16:54:13 -0400

    Use proper state management principles in register form, implement input validation

--- src/app/login/register/page.tsx
+++ src/app/login/register/page.tsx
@@ -1,56 +1,147 @@
-'use client';
+'use client';''
 
 import { Input } from "@/components/UI/Input/Input";
-import { isValidPassword } from "@/utility/functions/validation/isValidPassword";
-import { useEffect, useMemo, useState } from "react";
+import { useRef, useState } from "react";
 import styles from '../login.module.scss';
 import zxcvbn from "zxcvbn";
 import { isValidEmail } from "@/utility/functions/validation/isValidEmail";
 
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type Errors = {
+  nameError: string;
+  emailError: string;
+  passwordError: string;
+  confirmPasswordError: string;
+};
+
 export default function Page() {
+  const [formData, setFormData] = useState<FormData>({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
 
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [emailError, setEmailError] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [passwordError, setPasswordError] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [matchError, setMatchError] = useState<string>('');
+  const [errors, setErrors] = useState<Errors>({
+    nameError: '',
+    emailError: '',
+    passwordError: '',
+    confirmPasswordError: ''
+  });
 
-  const trimmed = useMemo(() => password.slice(0, 256), [password]);
-  const { score } = useMemo(() => zxcvbn(trimmed), [trimmed]);
+  const handleChange = (field: keyof FormData, value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [field]: value
+    }));
+    setErrors(prevErrors => ({
+      ...prevErrors,
+      [`${field}Error`]: '',
+    }));
+  };
 
-  const validEmail = useMemo(() => isValidEmail(email), [email]);
+  const validateField = (field: keyof FormData) => {
+    let errorMessage = '';
 
-  
-  
+    switch (field) {
+      case 'name':
+        if (!formData.name) {
+          errorMessage = 'Please enter a name';
+        }
+        break;
+      case 'email':
+        if (!formData.email || !isValidEmail(formData.email)) {
+          errorMessage = 'Please enter a valid email address';
+        }
+        break;
+      case 'password':
+        const trimmed = formData.password.slice(0, 256);
+        const { score } = zxcvbn(trimmed);
+        if (!formData.password) {
+          errorMessage = 'Please enter a password';
+        } else if (score < 2) {
+          errorMessage = 'Please use a stronger password';
+        }
+        break;
+      case 'confirmPassword':
+        if (formData.confirmPassword !== formData.password) {
+          errorMessage = 'Passwords do not match';
+        }
+        break;
+    }
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [`${field}Error`]: errorMessage
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    ['name', 'email', 'password', 'confirmPassword'].forEach((field) => validateField(field as keyof FormData));
+    if (Object.values(errors).some(v => !!v)) {
+      return;
+    }
+  };
+
   return (
     <div className={styles.form}>
       <p>Create an account</p>
-      <Input className={styles.input} value={name} onChange={(e) => setName(e.target.value)} placeholder="Your name" autoFocus />
-      <Input className={styles.input} value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Your email" />
-      <Input onBlur={() => score < 2 && setPasswordError('Please use a stronger password')} 
-        type='password' 
-        className={styles.input} 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-        placeholder="Your password" 
+      <Input
+        onBlur={() => validateField('name')}
+        className={styles.input}
+        value={formData.name}
+        onChange={(e) => handleChange('name', e.target.value)}
+        placeholder="Your name"
         errorOnFocusOnly={true}
-        errorMessage={passwordError}
+        errorMessage={errors.nameError}
+        required
+        autoFocus
       />
-      <Input onBlur={() => confirmPassword && password !== confirmPassword && setMatchError('Passwords do not match')} 
-        type='password' 
-        className={styles.input} 
-        value={confirmPassword} 
-        onChange={(e) => setConfirmPassword(e.target.value)} 
-        placeholder="Confirm password" 
-        errorMessage={matchError}
+      <Input
+        onBlur={() => validateField('email')}
+        className={styles.input}
+        value={formData.email}
+        onChange={(e) => handleChange('email', e.target.value)}
+        errorOnFocusOnly={true}
+        errorMessage={errors.emailError}
+        placeholder="Your email"
+        required
+      />
+      <Input
+        onBlur={() => validateField('password')}
+        type="password"
+        className={styles.input}
+        value={formData.password}
+        onChange={(e) => handleChange('password', e.target.value)}
+        placeholder="Your password"
+        errorOnFocusOnly={true}
+        errorMessage={errors.passwordError}
+        required
+      />
+      <Input
+        onBlur={() => validateField('confirmPassword')}
+        type="password"
+        className={styles.input}
+        value={formData.confirmPassword}
+        onChange={(e) => handleChange('confirmPassword', e.target.value)}
+        placeholder="Confirm password"
+        errorOnFocusOnly={true}
+        errorMessage={errors.confirmPasswordError}
+        required
       />
       <br />
-      <button>Create Account</button>
+      <button onClick={handleSubmit}>Create Account</button>
       <br />
 
-      <p>{'strength: ' + score}</p>
+      <p>{`strength: ${zxcvbn(formData.password.slice(0, 256)).score}`}</p>
     </div>
-  )
-} 
+  );
+}
