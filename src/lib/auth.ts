2bbd5561fb54e9f05c330f701b61e2d6e7f68f1f
Ryan1431 <ryan1431@gmail.com>
2023-05-20 07:57:40 -0400

    Remove sessionSample component

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,7 +12,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      
     }),
     CredentialsProvider({
       name: "Sign in",
