e6f274537f5da4c3103bc7f922099cbe6146fdbe
Ryan1431 <ryan1431@gmail.com>
2023-05-23 13:13:41 -0400

    Create loading state for Button ui component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -5,14 +5,20 @@ import styles from './button.module.scss';
 interface ButtonProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   icon?: JSX.Element,
   children?: React.ReactNode,
+  loading?: boolean,
 }
 
-export const Button: React.FC<ButtonProps> = ({icon, children, ...props}) => {
-
+export const Button: React.FC<ButtonProps> = ({icon, children, loading = true, ...props}) => {
+  
   return (
-    <div {...props} tabIndex={0} onKeyDown={(e) => e.key === 'Enter' && e.currentTarget.click()} className={clsx(styles.button, props.className ?? '')}>
+    <div {...props} 
+      tabIndex={0} 
+      onKeyDown={(e) => e.key === 'Enter' && e.currentTarget.click()} 
+      className={clsx(styles.button, props.className ?? '', {[styles.loading]: loading})}
+    >
       {icon && <div>{icon}</div>}
       <p>{children}</p>
+      <div className={styles.spinner}></div>
     </div>
   )
 }
