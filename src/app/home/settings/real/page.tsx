67b7b29e3c63cb45999e1a923bcefc59c4f93089
Ryan1431 <ryan1431@gmail.com>
2023-05-24 16:34:53 -0400

    Import minimum jsx for settings loading page

---
+++ src/app/home/settings/real/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+import { SettingsView } from "../settingsView";
+
+export default function Page() {
+
+  return (
+    <>
+      <Header text="Settings" />
+      <SettingsView />
+    </>
+  )
+}
