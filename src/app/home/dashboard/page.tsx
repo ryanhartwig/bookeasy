b759c3ef89a8beeddd2b4343824c8014712c98a0
Ryan1431 <ryan1431@gmail.com>
2023-05-24 12:42:08 -0400

    Add loading state for weely overview in dashboard

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,14 +1,31 @@
+import { Header } from "@/components/Header"
+import { SecondaryHeader } from "@/components/SecondaryHeader"
+import { Card } from "@/components/UI/Card/Card";
+import { SectionLabel } from "@/components/UI/SectionLabel/SectionLabel"
 import styles from './dashboard.module.scss';
-import { Header } from '@/components/Header';
-import DashboardView from './DashboardView';
+import { WeekDayNames } from "./weekDayNames";
 
 export default function Page() {
-  
   return (
     <>
-      <Header text='Dashboard' />
+      <Header text='Dashboard' loading />
+      <SecondaryHeader>
+      </SecondaryHeader>
       <div id="dashboard" className={styles.dashboard}>
-        <DashboardView />
+        <div className={styles.content}>
+          <SectionLabel label='Today' />
+          <div className={styles.appointments}>
+            <p className={styles.no_apps}>No appointments to show</p>
+          </div>
+          <SectionLabel label='This Week' />
+          <Card className={styles.weekview_card}>
+            <WeekDayNames />
+            <div className={styles.loadingblock } >
+              <div className={styles.loadingswipe} />
+              <div className={styles.loadingswipe} />
+            </div>
+          </Card>
+        </div>
       </div>
     </>
   )
