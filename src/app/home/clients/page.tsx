f0c7050514ec560dbf88cdff32a0a769eb235639
Ryan1431 <ryan1431@gmail.com>
2023-05-24 15:37:16 -0400

    Implement new client loading page

--- src/app/home/clients/page.tsx
+++ src/app/home/clients/page.tsx
@@ -1,29 +1,11 @@
 import { Header } from '@/components/Header';
 import { ClientsView } from './clientsView';
-import styles from './clients.module.scss';
-import { LoadingDots } from '@/components/UI/LoadingDots/LoadingDots';
 
 export default function Page() {
   return (
     <>
-      <Header text='Clients' loading />
-      <div className={styles.wrapper}>
-        <div className={styles.clients}>
-          <input 
-            value={''} 
-            className={styles.input}
-            placeholder='Search by client...'
-          />
-          <div className={styles.addClient}>
-            <p></p>
-            <LoadingDots style={{padding: 5}}/>
-          </div>
-
-          <LoadingDots style={{margin: 10, justifyContent: 'flex-start'}} />
-        </div>
-
-        <p style={{width: '100%', padding: 20, fontWeight: 300, fontSize: 14, color: 'grey'}}>Select a client to see details</p>
-      </div>
+      <Header text='Clients' />
+      <ClientsView />
     </>
   )
 }
