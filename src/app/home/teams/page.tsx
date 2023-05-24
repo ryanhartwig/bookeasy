0573dbbcd0129eec7452f02a0735f3fe827eb4f8
Ryan1431 <ryan1431@gmail.com>
2023-05-24 16:18:18 -0400

    Import jsx from relevant components within Teams to loading page

--- src/app/home/teams/page.tsx
+++ src/app/home/teams/page.tsx
@@ -1,12 +1,26 @@
 import { Header } from "@/components/Header";
-import { TeamsView } from "./teamsView";
-
+import styles from './teams.module.scss';
+import Image from "next/image";
+import addTeam from '@/assets/team_add.svg';
+import { LoadingDots } from "@/components/UI/LoadingDots/LoadingDots";
 
 export default function Page() {
   return (
     <>
-      <Header text="Teams" />
-      <TeamsView />
+      <Header text="Teams" loading />
+      <div className={styles.Teams}>
+        <div className={styles.teams_section}>
+          <p>My teams</p>
+          <div className={styles.user_teams}>
+            {/* Create team */}
+            <div className={styles.create_team} style={{pointerEvents: 'none'}}> 
+              <LoadingDots size={3} style={{padding: 8}} />
+              <Image src={addTeam} alt='Add team icon' className={styles.create_team_icon} />
+            </div>
+          </div>
+        </div>
+        
+      </div>
     </>
   )
 }
---
+++ src/app/home/teams/real/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+import { TeamsView } from "../teamsView";
+
+
+export default function Page() {
+  return (
+    <>
+      <Header text="Teams" />
+      <TeamsView />
+    </>
+  )
+}
