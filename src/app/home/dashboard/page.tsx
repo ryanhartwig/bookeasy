f30eaedf940faad399e21693ef1907c492994ea4
Ryan1431 <ryan1431@gmail.com>
2023-05-24 12:56:46 -0400

    Implement dashboard loading page

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,52 +1,15 @@
-import { Header } from "@/components/Header"
-import { SecondaryHeader } from "@/components/SecondaryHeader"
-import { Card } from "@/components/UI/Card/Card";
-import { SectionLabel } from "@/components/UI/SectionLabel/SectionLabel"
-import { Skeleton } from "@/components/UI/Skeleton/Skeleton";
-import { getCurrentWeek } from "@/utility/functions/dateRanges/getCurrentWeek";
+import { Header } from "@/components/Header";
+import DashboardView from "./DashboardView";
 import styles from './dashboard.module.scss';
-import { WeekDayNames } from "./weekDayNames";
 
 export default function Page() {
-  const [start, end] = getCurrentWeek();
   
   return (
     <>
-      <Header text='Dashboard' loading />
-      <SecondaryHeader>
-        <div className={styles.header}>
-        <div className={styles.stat}>
-          <p style={{fontWeight: 100}}>Week of</p>
-          <p className={styles.headerLarge}>
-            {`${start.toDateString().split(' ').slice(1, 3).join(' ')}`} - {`${end.toDateString().split(' ').slice(1, 3).join(' ')}`}
-          </p>
-        </div>
-        <div className={styles.stat}>
-          <p className={styles.headerLarge}>0</p>
-          <p style={{fontWeight: 100}}>Appointments</p>
-        </div>
-        <div className={styles.stat}>
-          <p className={styles.headerLarge}>$0.00</p>
-          <p style={{fontWeight: 100}}>Projected</p>
-        </div>
-        <div className={styles.addApointment}>
-          <Skeleton />
-        </div>
-      </div>
-      </SecondaryHeader>
+      <Header text='Dashboard' />
       <div id="dashboard" className={styles.dashboard}>
-        <div className={styles.content}>
-          <SectionLabel label='Today' />
-          <div className={styles.appointments}>
-            <p className={styles.no_apps}>No appointments to show</p>
-          </div>
-          <SectionLabel label='This Week' />
-          <Card className={styles.weekview_card}>
-            <WeekDayNames />
-            <Skeleton />
-          </Card>
-        </div>
+        <DashboardView />
       </div>
     </>
   )
-}
+} 
