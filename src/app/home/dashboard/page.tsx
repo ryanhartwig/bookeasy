f47f819a97396f94012df1aafa1acd9ae3de25d9
Ryan1431 <ryan1431@gmail.com>
2023-05-24 12:53:25 -0400

    Import raw jsx from Stats component for loading page

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -3,14 +3,36 @@ import { SecondaryHeader } from "@/components/SecondaryHeader"
 import { Card } from "@/components/UI/Card/Card";
 import { SectionLabel } from "@/components/UI/SectionLabel/SectionLabel"
 import { Skeleton } from "@/components/UI/Skeleton/Skeleton";
+import { getCurrentWeek } from "@/utility/functions/dateRanges/getCurrentWeek";
 import styles from './dashboard.module.scss';
 import { WeekDayNames } from "./weekDayNames";
 
 export default function Page() {
+  const [start, end] = getCurrentWeek();
+  
   return (
     <>
       <Header text='Dashboard' loading />
       <SecondaryHeader>
+        <div className={styles.header}>
+        <div className={styles.stat}>
+          <p style={{fontWeight: 100}}>Week of</p>
+          <p className={styles.headerLarge}>
+            {`${start.toDateString().split(' ').slice(1, 3).join(' ')}`} - {`${end.toDateString().split(' ').slice(1, 3).join(' ')}`}
+          </p>
+        </div>
+        <div className={styles.stat}>
+          <p className={styles.headerLarge}>0</p>
+          <p style={{fontWeight: 100}}>Appointments</p>
+        </div>
+        <div className={styles.stat}>
+          <p className={styles.headerLarge}>$0.00</p>
+          <p style={{fontWeight: 100}}>Projected</p>
+        </div>
+        <div className={styles.addApointment}>
+          <Skeleton />
+        </div>
+      </div>
       </SecondaryHeader>
       <div id="dashboard" className={styles.dashboard}>
         <div className={styles.content}>
