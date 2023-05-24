d1cfc2e83cff43ed6cde67cddeae0b3cf68b28e0
Ryan1431 <ryan1431@gmail.com>
2023-05-24 12:27:13 -0400

    Dashboard loading page

---
+++ src/app/home/dashboard/DashboardView.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import styles from './dashboard.module.scss';
+import { Appointments } from './appointments';
+import { WeekDayNames } from './weekDayNames';
+import { WeekDays } from './weekDays';
+import { Card } from '@/components/UI/Card/Card';
+import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
+import { Stats } from './stats';
+import { SecondaryHeader } from '@/components/SecondaryHeader';
+import { useUser } from '@/app/Providers';
+
+export default function DashboardView() {
+  const { id } = useUser();
+  
+  return (
+    <>
+      <SecondaryHeader>
+        <Stats userId={id} />
+      </SecondaryHeader>
+      <div className={styles.content}>
+        <SectionLabel label='Today' />
+        <Appointments userId={id}/>
+        <SectionLabel label='This Week' />
+        <Card className={styles.weekview_card}>
+          <WeekDayNames />
+          <WeekDays userId={id} />
+        </Card>
+      </div>
+    </>
+  )
+}
--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,35 +1,14 @@
-'use client';
-
 import styles from './dashboard.module.scss';
-import { Appointments } from './appointments';
-import { WeekDayNames } from './weekDayNames';
-import { WeekDays } from './weekDays';
-import { Card } from '@/components/UI/Card/Card';
-import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
 import { Header } from '@/components/Header';
-import { Stats } from './stats';
-import { SecondaryHeader } from '@/components/SecondaryHeader';
-import { useUser } from '@/app/Providers';
+import DashboardView from './DashboardView';
 
 export default function Page() {
-  const { id } = useUser();
   
   return (
     <>
       <Header text='Dashboard' />
       <div id="dashboard" className={styles.dashboard}>
-        <SecondaryHeader>
-          <Stats userId={id} />
-        </SecondaryHeader>
-        <div className={styles.content}>
-          <SectionLabel label='Today' />
-          <Appointments userId={id}/>
-          <SectionLabel label='This Week' />
-          <Card className={styles.weekview_card}>
-            <WeekDayNames />
-            <WeekDays userId={id} />
-          </Card>
-        </div>
+        <DashboardView />
       </div>
     </>
   )
