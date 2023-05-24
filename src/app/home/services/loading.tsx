1cf09e05694c14f53dc3f3898ad1a92ae13b3415
Ryan1431 <ryan1431@gmail.com>
2023-05-24 15:42:22 -0400

    Modify services loading page

--- src/app/home/services/loading.tsx
+++ src/app/home/services/loading.tsx
@@ -1,16 +1,20 @@
 import { Header } from "@/components/Header"
+import { LoadingDots } from "@/components/UI/LoadingDots/LoadingDots";
 import styles from './services.module.scss';
 
 export default function Loading() {
   
-  return <>
-    <Header text="Services" loading />
-    <div className={styles.services_page}>
-      <div className={styles.labels}>
-        {['Name', 'Duration', 'Provider', 'Assignee(s)', 'Cost'].map((t) => 
-          <p key={t}>{t}</p>
-        )}
+  return (
+    <>
+      <Header text="Services" loading />
+      <div className={styles.services_page}>
+        <div className={styles.labels}>
+          {['Name', 'Duration', 'Provider', 'Assignee(s)', 'Cost'].map((t) => 
+            <p key={t}>{t}</p>
+          )}
+        </div>
+        <LoadingDots size={4} style={{justifyContent: 'flex-start', margin: '15px 30px'}} />
       </div>
-    </div>
-  </>
+    </>
+  )
 }
--- src/app/home/services/page.tsx
+++ src/app/home/services/page.tsx
@@ -2,7 +2,6 @@ import { Header } from '@/components/Header';
 import { ServicesView } from './ServicesView';
 
  export default async function Page() {
-  // await (() => new Promise((res) => setTimeout(() => res(''), 120000)))()
   
   return (
     <>
