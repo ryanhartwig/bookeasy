f27c4f7aef93fdfcbce9a188c0d93c65196ccbe3
Ryan1431 <ryan1431@gmail.com>
2023-05-24 15:36:05 -0400

    Import necessary HTML elements into client template loading page

---
+++ src/app/home/clients/real/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/Header';
+import { ClientsView } from '../clientsView';
+
+export default function Page() {
+  return (
+    <>
+      <Header text='Clients' />
+      <ClientsView />
+    </>
+  )
+}
