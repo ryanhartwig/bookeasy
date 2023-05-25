fdafc5a6915854e12f783676ab03633c142e6375
Ryan1431 <ryan1431@gmail.com>
2023-05-25 13:00:13 -0400

    Use map function to display logo spans, which animate out with a index based delay when no longer loading

--- src/components/UI/LoadingSplash/LoadingSplash.tsx
+++ src/components/UI/LoadingSplash/LoadingSplash.tsx
@@ -11,12 +11,25 @@ interface LoadingSplashProps extends React.DetailedHTMLProps<React.HTMLAttribute
 
 export const LoadingSplash: React.FC<LoadingSplashProps> = ({loading = false, ...props}) => {
 
+  const bookeasy = ['b', 'o', 'o', 'k', 'e', 'a', 's', 'y'].reverse();
+
   return (
     <div {...props} className={clsx(styles.loadingCover, props.className || '')}>
       <div className={styles.loadingSplash}>
         <div className={styles.logo}>
-          <Image priority src={bookit.default} alt="Book it logo" />
-          <p><span>book</span>easy.</p>
+          <Image className={clsx({[styles.outright]: !loading})} 
+            priority 
+            style={{animationDelay: '200ms'}}
+            src={bookit.default} 
+            alt="Book it logo" 
+          />
+          <p>
+            {bookeasy.map((c, i) => 
+              <span className={clsx({[styles.outright]: !loading})} style={{animationDelay: `${(i * 25)}ms`}}
+                key={`${c}${i}`}
+              >{c}</span>
+            )}
+          </p>
         </div>
         {loading && <div className={styles.loadingSpinner} >
           <Spinner style={{position: 'relative', top: 15, left: 5}} />
