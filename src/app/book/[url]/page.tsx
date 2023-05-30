6cbc734feb56153fabe86fb117eafbb240b2b89f
Ryan1431 <ryan1431@gmail.com>
2023-05-30 13:22:28 -0400

    Create notfound component and use Loading from local directory to minimize JSX in page.tsx

--- src/app/book/[url]/page.tsx
+++ src/app/book/[url]/page.tsx
@@ -5,29 +5,15 @@ import { GET_BOOKING_SITE } from '@/utility/queries/bookingSiteQueries';
 import { useQuery } from '@apollo/client';
 import { useMemo } from 'react';
 import styles from './book.module.scss';
+import Loading from './loading';
+import { NotFound } from './notfound';
 
 export default function Page({params}: { params: any }) {
   const { data: bookingSiteData, loading: loadingBookingSiteData } = useQuery(GET_BOOKING_SITE, { variables: { url: params.url }});
 
   const loading = useMemo(() => loadingBookingSiteData, [loadingBookingSiteData]);
-  if (loading) return (
-    <div className={styles.background}>
-      <LoadingSplash loading />
-    </div>  
-  );
-
-  if(!bookingSiteData?.getBookingSite) return (
-    <div className={styles.background}>
-      <div className={styles.notfound}>
-        <div className={styles.errorcode}>
-          <hr />
-          <p className={styles.fourohfour}>404</p>
-          <hr />
-        </div>
-        <p>The booking page URL may have changed or no longer exists.</p>
-      </div>
-    </div>
-  );
+  if (loading) return <Loading />
+  if(!bookingSiteData?.getBookingSite) return <NotFound />
 
   return (
     <div className={styles.background}>
