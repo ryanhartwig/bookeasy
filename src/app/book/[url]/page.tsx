78ac5ca3ac37b261b3a957aadd7b9e506769bc22
Ryan1431 <ryan1431@gmail.com>
2023-05-30 14:02:01 -0400

    Show avatar conditionally if present, overlap header and change business name margin

--- src/app/book/[url]/page.tsx
+++ src/app/book/[url]/page.tsx
@@ -1,24 +1,29 @@
 'use client';
 
+import { Avatar } from '@/components/UI/Avatar/Avatar';
 import { Card } from '@/components/UI/Card/Card';
+import { NewBusiness } from '@/types/Business';
 import { GET_BOOKING_SITE } from '@/utility/queries/bookingSiteQueries';
 import { GET_BUSINESS } from '@/utility/queries/businessQueries';
 import { useQuery } from '@apollo/client';
 import clsx from 'clsx';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import styles from './book.module.scss';
 import Loading from './loading';
 import { NotFound } from './notfound';
 
 export default function Page({params}: { params: any }) {
   const [tab, setTab] = useState('Book');
+  const [business, setBusiness] = useState<NewBusiness>();
 
   const { data: bookingSiteData, loading: loadingBookingSiteData } = useQuery(GET_BOOKING_SITE, { variables: { url: params.url }});
   const { data: businessData, loading: loadingBusinessData } = useQuery(GET_BUSINESS, { variables: { businessId: bookingSiteData?.getBookingSite?.business_id }, skip: !bookingSiteData?.getBookingSite })
-  const loading = useMemo(() => loadingBookingSiteData, [loadingBookingSiteData]);
+  useEffect(() => businessData && setBusiness(businessData.getBusiness), [businessData]);
+
+  const loading = useMemo(() => loadingBookingSiteData || loadingBusinessData, [loadingBookingSiteData, loadingBusinessData]);
 
   if (loading) return <Loading />
-  if(!bookingSiteData?.getBookingSite) return <NotFound />
+  if(!bookingSiteData?.getBookingSite || !business) return <NotFound />
   return (
     <div className={styles.background}>
       <div className={styles.header}>
@@ -26,7 +31,8 @@ export default function Page({params}: { params: any }) {
       </div>
       <div className={styles.page}>
         <div className={styles.title}>
-          <h2>{businessData?.getBusiness.name}</h2>
+          {business.avatar && <Avatar className={styles.businessPhoto} size={150} src={business.avatar} />}
+          <h2 style={{marginLeft: business.avatar ? '150px' : ''}}>{business.name}</h2>
           <hr />
         </div>
         <div className={clsx(styles.navigation, 'noselect')}>
