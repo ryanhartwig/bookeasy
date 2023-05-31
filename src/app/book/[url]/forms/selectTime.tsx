9dbac003cf1217b805564c436e5b1bf398fa29bb
Ryan1431 <ryan1431@gmail.com>
2023-05-31 09:53:28 -0400

    Install react-calendar and import into selectTime component

--- src/app/book/[url]/forms/selectTime.tsx
+++ src/app/book/[url]/forms/selectTime.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
 import styles from './forms.module.scss';
+import './calendar.css';
 
 export const SelectTime = () => {
 
+  const [value, onChange] = useState(new Date());
+
   return (
     <div className={styles.selectTime}>
-      <p>select a time</p>
+      <Calendar 
+        tileDisabled={({activeStartDate, date, view}) => {
+          console.log(date)
+          return false
+        }}
+        minDate={new Date()} 
+        onChange={(value, e) => {console.log(value)}} 
+        value={value} 
+      />
     </div>
   )
 }
