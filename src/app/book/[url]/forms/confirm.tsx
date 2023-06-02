71106544c17443ecb4dcbdcd1764a7d78df5c76a
Ryan1431 <ryan1431@gmail.com>
2023-06-02 17:50:19 -0400

    Show agent and service card on confirmation screen

--- src/app/book/[url]/forms/confirm.tsx
+++ src/app/book/[url]/forms/confirm.tsx
@@ -1,3 +1,6 @@
+import { Avatar } from '@/components/UI/Avatar/Avatar';
+import { Card } from '@/components/UI/Card/Card';
+import { TextButton } from '@/components/UI/TextButton/TextButton';
 import { AppointmentInput } from '@/types/Appointment';
 import { useMemo } from 'react';
 import uuid from 'react-uuid';
@@ -33,7 +36,35 @@ export const Confirm: React.FC<ConfirmProps> = ({selected, businessId}) => {
   
   return (
     <div className={styles.confirm}>
-      <p>confirm</p>
+      <div className={styles.header}>
+        <hr />
+        <p>Confirm Appointment Details</p>
+        <hr />
+      </div>
+      <div className={styles.overview}>
+        <div className={styles.agent}>
+          <p>Your agent</p>
+          <Card className={styles.agentCard}>
+            <Avatar src={selected.staff!.avatar} size={114} />
+            <p className={styles.name}>{selected.staff!.name}</p>
+            <p className={styles.credentials}>Credentials</p>
+          </Card>
+        </div>
+        <div className={styles.service}>
+          <p>Your Service</p>
+          <Card className={styles.serviceCard} style={{borderColor: selected.service!.color}}>
+            <div className={styles.serviceDetails}>
+              <p>{selected.service!.name}</p>
+              <p className={styles.serviceDuration}>{selected.service!.duration} min</p>
+            </div>
+            <p className={styles.date}>{selected.startDate!.toDateString()}</p>
+            <p className={styles.cost}>${selected.service!.cost.toFixed(2)}</p>
+          </Card>
+        </div>
+      </div>
+      <div className={styles.confirmButton}>
+        <TextButton>Confirm Booking</TextButton>
+      </div>
     </div>
   )
 }
