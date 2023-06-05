af748f8856ed191e952710ffdadb1087725c5199
Ryan1431 <ryan1431@gmail.com>
2023-06-05 18:22:11 -0400

    Show contact links, bio conditionally

--- src/app/book/[url]/about.tsx
+++ src/app/book/[url]/about.tsx
@@ -1,7 +1,8 @@
 import styles from './book.module.scss';
 import { NewBusiness } from "@/types/Business"
 import { SectionLabel } from '@/components/UI/SectionLabel/SectionLabel';
-import { Avatar } from '@/components/UI/Avatar/Avatar';
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
+import { TbWorldWww } from 'react-icons/tb';
 
 interface AboutProps {
   business: NewBusiness,
@@ -11,17 +12,26 @@ export const About: React.FC<AboutProps> = ({business}) => {
 
   return (
     <div className={styles.contact}>  
-      <SectionLabel label={`Contact ${business.name}`} />
+      <SectionLabel label={`About ${business.name}`} />
+      {false // to be business.bio
+        ? <p className={styles.bio}></p>
+        : <p className={styles.nobio}>No details.</p>
+      }
+      
+      <SectionLabel label={`Contact`} />
       <div className={styles.contactLink}>
+        <AiOutlinePhone />
         <p>{business.phone ?? 'None'}</p>
       </div>
       <div className={styles.contactLink}>
+        <AiOutlineMail className={styles.icon} />
         {business.email 
           ? <a href={`mailto:${business.email}`}>{business.email}</a>
           : <p>None</p>
         }
       </div>
       <div className={styles.contactLink}>
+        <TbWorldWww />
         {false // to be business.website 
           ? <a href={''} target={"_blank"}>http://www.tobeimplemented.ca</a>
           : <p>None</p>
