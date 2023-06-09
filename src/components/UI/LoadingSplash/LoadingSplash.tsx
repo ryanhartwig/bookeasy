fcbcfb989ae0a34ab7e3be9e9aa2b7552f3f724f
Ryan1431 <ryan1431@gmail.com>
2023-06-09 16:49:09 -0400

    Add logo transition fade in client login view

--- src/components/UI/LoadingSplash/LoadingSplash.tsx
+++ src/components/UI/LoadingSplash/LoadingSplash.tsx
@@ -25,7 +25,7 @@ export const LoadingSplash: React.FC<LoadingSplashProps> = ({loading = false, sk
           />
           <p>
             {bookeasy.map((c, i) => 
-              <span className={clsx({[styles.outright]: !loading && !skipTranslate}, {[styles.fadeup]: loading})} style={{animationDelay: !loading ? `${(i * 25)}ms` : '0ms'}}
+              <span className={clsx({[styles.outright]: !loading && !skipTranslate}, {[styles.fadeup]: loading})} style={{animationDelay: !loading ? `${(i * 30)}ms` : '0ms'}}
                 key={`${c}${i}`}
               >{c}</span>
             )}
