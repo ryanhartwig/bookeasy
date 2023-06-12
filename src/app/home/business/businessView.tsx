f9f60ae6729a80bdf3b9be3d2beb1e75e46f47bc
Ryan1431 <ryan1431@gmail.com>
2023-06-12 08:45:27 -0400

    Implement mutation to create a business when new user clicks on My Business view

--- src/app/home/business/businessView.tsx
+++ src/app/home/business/businessView.tsx
@@ -1,16 +1,28 @@
 'use client';
 
 import { useUser } from "@/app/Providers";
-import { GET_BUSINESS_WITH_STAFF_ID } from "@/utility/queries/businessQueries";
-import { GET_USER } from "@/utility/queries/userQueries";
-import { useQuery } from "@apollo/client";
+import { GET_BUSINESS_WITH_STAFF_ID, NEW_BUSINESS } from "@/utility/queries/businessQueries";
+import { GET_USER, PATCH_USER } from "@/utility/queries/userQueries";
+import { useMutation, useQuery } from "@apollo/client";
+import { useEffect } from "react";
 import { Business } from "./business";
 import Loading from "./loading";
 
 export const BusinessView = () => {
   const { id } = useUser();
-  const { data: userData } = useQuery(GET_USER, { variables: { userId: id }});
-  const { data: businessData } = useQuery(GET_BUSINESS_WITH_STAFF_ID, { variables: { businessId: userData?.getUser?.business_id }, skip: !userData});
+  const { data: userData, loading: loadingUserData } = useQuery(GET_USER, { variables: { userId: id }});
+  const { data: businessData } = useQuery(GET_BUSINESS_WITH_STAFF_ID, { variables: { businessId: userData?.getUser?.business_id }, skip: !userData?.getUser?.business_id});
+
+  const [createBusiness] = useMutation(NEW_BUSINESS);
+  const [patchUser] = useMutation(PATCH_USER, { refetchQueries: [ GET_USER ]});
+  useEffect(() => {
+    if (loadingUserData || userData?.getUser?.business_id) return;
+
+    ;(async () => {
+      const { data: businessData } = await createBusiness({ variables: { name: userData.getUser.name, userId: userData.getUser.id }});
+      await patchUser({ variables: { userId: id, patch: { business_id: businessData.newBusiness.id }}})
+    })();
+  }, [createBusiness, id, loadingUserData, patchUser, userData]);
 
   return (
     <>
