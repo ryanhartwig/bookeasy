c888917c7ec3e2356bbf28939710084f9bd71eaa
Ryan1431 <ryan1431@gmail.com>
2023-06-12 20:17:16 -0400

    Reset copy link state after 3 seconds of clicking

--- src/components/business_settings/BookingSiteOverview.tsx
+++ src/components/business_settings/BookingSiteOverview.tsx
@@ -24,6 +24,7 @@ export const BookingSiteOverview: React.FC<BookingSiteOverviewProps> = ({ bookin
     ;(async () => {
       await navigator.clipboard.writeText(url);
       setCopied(true);
+      setTimeout(() => setCopied(false), 3000);
     })();
   }, [url]);
   
